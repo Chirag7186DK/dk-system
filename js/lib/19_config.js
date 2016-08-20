9c6b641f91aa4fd3a0c673cab902e4d6c28ff849
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-20 15:32:52 +0530

    code committed related to dk system data

--- js/lib/19_config.js
+++ js/lib/19_config.js
@@ -16,3 +16,52 @@ console.log("on load deviceWidth=>"+deviceWidth+", deviceHeight=>"+deviceHeight)
 
 // web-app module defined here 
 var app = angular.module('DESSERTSKHAZANAAPP', []);
+
+// userSessionController using
+app.controller('userSessionController', function($scope, $rootScope, UsersServices, OrderCartServices){
+    
+    // global variable declare 
+    // this variable will be reflect on ui screen
+    $rootScope.isDkDeliveryCityChanged = false;
+    $rootScope.isDkDeliveryAreaChanged = false;
+    $rootScope.dkDeliveryCityList = false;
+    $rootScope.defaultedSelectedDKDeliveryCity = '';
+    $rootScope.dkDeliveryAreaList = false;
+    $rootScope.defaultedSelectedDKDeliveryArea = '';
+    $rootScope.dkDeliveryAreaBasedProductTypeList = false;
+    $rootScope.defaultedSelectedDKDeliveryAreaBasedProductTypeValue = '';
+    $rootScope.defaultedSelectedDKDeliveryAreaBasedProductTypeTitle = '';
+    $rootScope.isDessertsProductTypeProductListLoaded = false;
+    
+    // loadDefaultDataInDkSession
+    $rootScope.loadDefaultDataInDkSession = function(pageLoad){
+        try{
+            // check DKPARAMOBJ is available or not in session storage
+            if((sessionStorage.getItem('DKPARAMOBJ')===null || sessionStorage.getItem('DKPARAMOBJ')===undefined 
+                || sessionStorage.getItem('DKPARAMOBJ')==='' || sessionStorage.getItem('DKPARAMOBJ')===false) && pageLoad==='home'){
+                var retStatusResetDkSessionDataCleared = initializeDkSessionData();  
+                if(retStatusResetDkSessionDataCleared===true){
+                }
+            }else if((sessionStorage.getItem('DKPARAMOBJ')!==null && sessionStorage.getItem('DKPARAMOBJ')!==undefined 
+                && sessionStorage.getItem('DKPARAMOBJ')!=='' && sessionStorage.getItem('DKPARAMOBJ')!==false) && pageLoad==='home'){
+                // generate new user session id
+                generateDkUserSessionId();
+                addTrackingUserInfoAccessingWebsitesDetails(pageLoad);
+            }else if((sessionStorage.getItem('DKPARAMOBJ')===null || sessionStorage.getItem('DKPARAMOBJ')===undefined 
+                || sessionStorage.getItem('DKPARAMOBJ')==='' || sessionStorage.getItem('DKPARAMOBJ')===false) && pageLoad!=='home'){
+                // clear browser cookies and other thing by javascript
+                resetDKSessionData();
+                // redirect to main page of desserts khazana
+                window.location = globalBaseSitePath;
+            }
+        }catch(ex){
+            console.log("problem in loadDefaultDataInDkSession ex=>"+ex);
+        }
+    };
+    
+    // checkInBackgroundDataAvailableForUpdationFromSession
+    $rootScope.checkInBackgroundDataAvailableForUpdationFromSession = function(pageLoad){
+        OrderCartServices.addProductDataInOrdercart(false, false, 'session');
+    };
+    
+});
