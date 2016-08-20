3737942149b43b2cd708d1b53a1f50204fc8567f
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-20 20:45:57 +0530

    code committed related to dk system data

--- viewproduct.php
+++ viewproduct.php
@@ -21,7 +21,7 @@
 
     <!-- body start here -->
     
-    <body ng-cloak scroll-window-directive resize-window-directive class="ng-cloak onBodyScrollClass" ng-controller='userSessionController' ng-init="loadDefaultDataInDkSession('viewproduct'); checkInBackgroundDataAvailableForUpdationFromSession('viewproduct');">
+    <body ng-cloak scroll-window-directive resize-window-directive class="ng-cloak onBodyScrollClass" ng-controller='dkSessionController' ng-init="loadDefaultDataInDkSession('viewproduct'); checkInBackgroundDataAvailableForUpdationFromSession('viewproduct');">
         
         <!-- common SCROLL TOP BUTTON -->
         <a class="scrollToTopBtnClass" ng-show="isShowScrollToTopBtnWebAppPage" href="#" title='Click to scroll up page'>
