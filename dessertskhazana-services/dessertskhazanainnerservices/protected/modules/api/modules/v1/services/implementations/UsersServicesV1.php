d3677c94104873b5eeb7275986541cee1e34b8b8
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-20 21:19:20 +0530

    code committed related to dk system data

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/UsersServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/UsersServicesV1.php
@@ -58,7 +58,7 @@ class UsersServicesV1 implements IUsersServicesV1{
                         $rspDetails['userDetails']['udblogId'] = MD5($retLastInsertedUserInfoLogId);
                         $rspDetails['userDetails']['user_sessionid'] = $dkParamDataArr['user_sessionid'];
                         $rspDetails['userDetails']['user_sessionstarttime'] = $dkParamDataArr['user_sessionstarttime'];
-                        $rspDetails['userDetails']['userProfileTypeId'] = $dkParamDataArr['unmd5ProfileTypeId'];
+                        $rspDetails['userDetails']['userProfileTypeId'] = $userAuthenticationDetailsArr[0]['unmd5ProfileTypeId'];
                     }
                 }
             }else{
