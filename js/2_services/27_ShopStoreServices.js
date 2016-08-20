0b00abdf7979a65410aca21e581c6846841b0a51
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-20 18:58:25 +0530

    code committed related to dk system data

---
+++ js/2_services/27_ShopStoreServices.js
@@ -0,0 +1,28 @@
+
+// CJ defined this function 2016-06-22
+app.factory('ShopStoreServices', function($http, $q){
+    try{
+        var shopstoreDetails = {};
+        
+        shopstoreDetails.getCShopStoreSummaryInfo = function(preparedParamJsonObj){
+            var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/ShopStore/CShopStoreSummaryInfo", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+            return promiseObject;
+        };
+        shopstoreDetails.getCShopStoreProductTypeProductCategoryAllProductList = function(preparedParamJsonObj){
+            var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/ShopStore/CShopStoreProductTypeProductCategoryAllProductDetails", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+            return promiseObject;
+        };
+        shopstoreDetails.getCShopStoreWorkingStyleDetails = function(preparedParamJsonObj){
+            var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/ShopStore/CshopstoreWorkingstyle", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+            return promiseObject;
+        };
+        shopstoreDetails.getCShopStoreProductDeliveryAreaDetails = function(preparedParamJsonObj){
+            var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/ShopStore/CshopstoreProductdeliveryAreaInfo", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+            return promiseObject;
+        };
+        return shopstoreDetails;
+    }catch(ex){
+        console.log("problem in Shopstore services ex=>"+ex);
+        return false;
+    }
+}); 
