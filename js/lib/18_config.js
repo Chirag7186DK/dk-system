49e6a01463017135aa383f386ec7f247557d26d5
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-20 15:23:20 +0530

    Lib file commited

---
+++ js/lib/18_config.js
@@ -0,0 +1,8 @@
+
+isHeaderExecutingOnServerLevel = 'Y';
+apiUrlAccessHashValue = 'trr36pdthb9xbhcppyqkgbpkq';
+globalBaseSitePath = 'http://localhost/dk-system/';
+//globalBaseSitePath = 'http://192.168.1.103/dk-system/';
+product_year = '2016';
+isProductInMaintainanceMode = 'N';
+isDisableDocumentRightClick = 'Y';
