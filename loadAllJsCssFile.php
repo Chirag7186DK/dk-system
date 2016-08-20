2d55ef1e118366c68f9e9e2d46e2b08714cf3f55
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-20 19:01:17 +0530

    code committed related to dk system data

--- loadAllJsCssFile.php
+++ loadAllJsCssFile.php
@@ -11,7 +11,7 @@
     }
     
     // CJ defined this function to collect files in array
-    function collectfiles($dirname, $filetype){
+    function collectCssFiles($dirname, $filetype){
         $dirname = rtrim($dirname, '\\/');
         $outputFileList = array();
         $ffs = scandir($dirname);
@@ -33,7 +33,7 @@
     }
     
     // collect all css files 
-    $css_filelist = collectfiles("css", "css");
+    $css_filelist = collectCssFiles("css", "css");
     if(count($css_filelist)>0 && $css_filelist!=false && $css_filelist!=null){
         $retStatusSortedFiles = usort($css_filelist , 'sortFilesNumberically');
         // iterate each css files to load in dom
@@ -48,11 +48,32 @@
         }
     }
     
+    // CJ defined this function to collect files in array
+    function collectJsFiles($dirname, $filetype){
+        $dirname = rtrim($dirname, '\\/');
+        $outputFileList = array();
+        $ffs = scandir($dirname);
+        foreach($ffs as $ff) {
+            if($ff!='.' && $ff!= '..' && $ff!='.git'){
+                if(is_dir($dirname."/".$ff)){
+                    $collectedFiles = collectfiles("$dirname/$ff", $filetype);
+                    if($collectedFiles!=''){
+                       //array_push($outputFileList, $collectedFiles);
+                    }
+                }else{
+                    if(strrpos($ff, '.'.$filetype)==(strlen($ff) - strlen('.' . $filetype))){
+                        array_push($outputFileList, $ff);
+                    }
+                }
+            }
+        }
+        return $outputFileList;
+    }
+    
     // collect all js files
-    $jsFileList = collectfiles("js", "js");
+    $jsFileList = collectJsFiles("js", "js");
     if(count($jsFileList)>0 && $jsFileList!=false && $jsFileList!=null){
         $retStatusSortedFiles = usort($jsFileList , 'sortFilesNumberically');
-        //echo "<script src='http://maps.googleapis.com/maps/api/js?libraries=places'></script>";
         for($count = 0; $count<count($jsFileList); $count++){
             $filename = $jsFileList[$count];
             if(($filename!=".") && ($filename!="..") && ($filename!="")){
