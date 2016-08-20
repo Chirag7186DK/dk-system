f527c5b6ecbcc0707643e9694e38be505c829d1b
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-20 14:09:17 +0530

    code committed related to dk system data

--- viewproduct.php
+++ viewproduct.php
@@ -228,7 +228,6 @@
             <!-- create horizontally space div between -->
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 commonHorizontalSpaceDivClass"></div>
 
-            
             <!-- product description details -->
             <div ng-controller='ProductTypeProductCategoryProductDetailsController' ng-init="loadProductDescriptionDetails()" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 vpd_featuresContainerDivClass" ng-show="isProductDescriptionDetailsFound">
                 <div class="panel vpd_productDescriptionPanelContainerDivClass">
