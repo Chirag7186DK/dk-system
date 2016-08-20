5c4db80cdd73ae56c439ae35f2fa5683a9edf066
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-20 20:44:25 +0530

    code committed related to dk system data

--- js/3controller/41_CoportateOrderController.js
+++ js/3controller/41_CoportateOrderController.js
@@ -1,49 +1,82 @@
 
-// dkSessionController using
-app.controller('dkSessionController', function($scope, $rootScope, UsersServices, OrderCartServices){
-    
-    // global variable declare 
-    // this variable will be reflect on ui screen
-    $rootScope.isDkDeliveryCityChanged = false;
-    $rootScope.isDkDeliveryAreaChanged = false;
-    $rootScope.dkDeliveryCityList = false;
-    $rootScope.defaultedSelectedDKDeliveryCity = '';
-    $rootScope.dkDeliveryAreaList = false;
-    $rootScope.defaultedSelectedDKDeliveryArea = '';
-    $rootScope.dkDeliveryAreaBasedProductTypeList = false;
-    $rootScope.defaultedSelectedDKDeliveryAreaBasedProductTypeValue = '';
-    $rootScope.defaultedSelectedDKDeliveryAreaBasedProductTypeTitle = '';
-    $rootScope.isDessertsProductTypeProductListLoaded = false;
-    
-    // loadDefaultDataInDkSession
-    $rootScope.loadDefaultDataInDkSession = function(pageLoad){
-        try{
-            // check DKPARAMOBJ is available or not in session storage
-            if((sessionStorage.getItem('DKPARAMOBJ')===null || sessionStorage.getItem('DKPARAMOBJ')===undefined 
-                || sessionStorage.getItem('DKPARAMOBJ')==='' || sessionStorage.getItem('DKPARAMOBJ')===false) && pageLoad==='home'){
-                var retStatusResetDkSessionDataCleared = initializeDkSessionData();  
-                if(retStatusResetDkSessionDataCleared===true){
+// CorporateTieupController
+app.controller('CorporateTieupController', function($scope, $rootScope, $http, CorporateTieupServices){
+    try{
+        
+        $rootScope.isShowCorporateTieupRequestErrorMsg = false;
+        $rootScope.isShowCorporateTieupRequestFormContent = true;
+        $rootScope.isShowCorporateTieupRequestSendThankyouMsg = false;
+        $rootScope.corporateTieupErrorMsgStr = '';
+        
+        // redirectToViewCorporateTieupRequest 
+        $rootScope.redirectToViewCorporateTieupRequest = function(){
+            try{
+                // get param obj
+                var retStatus = checkParamDataToRedirectForRequestCorporateTieup();
+                // console.log("redirectToViewCorporateTieupRequest retStatus=>"+retStatus);
+                if(retStatus===true){
+                    window.location.href =  globalBaseSitePath+"corporatetieup.php";
                 }
-            }else if((sessionStorage.getItem('DKPARAMOBJ')!==null && sessionStorage.getItem('DKPARAMOBJ')!==undefined 
-                && sessionStorage.getItem('DKPARAMOBJ')!=='' && sessionStorage.getItem('DKPARAMOBJ')!==false) && pageLoad==='home'){
-                // generate new user session id
-                generateDkUserSessionId();
-                addTrackingUserInfoAccessingWebsitesDetails(pageLoad);
-            }else if((sessionStorage.getItem('DKPARAMOBJ')===null || sessionStorage.getItem('DKPARAMOBJ')===undefined 
-                || sessionStorage.getItem('DKPARAMOBJ')==='' || sessionStorage.getItem('DKPARAMOBJ')===false) && pageLoad!=='home'){
-                // clear browser cookies and other thing by javascript
-                resetDKSessionData();
-                // redirect to main page of desserts khazana
-                window.location = globalBaseSitePath;
+            }catch(ex){
+                console.log("problem in redirectToViewCorporateTieupRequest ex=>"+ex);
             }
-        }catch(ex){
-            console.log("problem in loadDefaultDataInDkSession ex=>"+ex);
-        }
-    };
-    
-    // checkInBackgroundDataAvailableForUpdationFromSession
-    $rootScope.checkInBackgroundDataAvailableForUpdationFromSession = function(pageLoad){
-        OrderCartServices.addProductDataInOrdercart(false, false, 'session');
-    };
-    
+        };
+        
+        // attachedFieldValidationCorporateTieupOrdersRequest
+        $rootScope.attachedFieldValidationCorporateTieupRequest = function(){
+            try{
+                attachedFieldValidationCorporateTieupRequest();
+            }catch(ex){
+                console.log("problem in attachedFieldValidationCorporateTieupRequest ex=>"+ex);
+            }
+        };
+        
+        // addCorporateTieupRequest 
+        $rootScope.addCorporateTieupRequest = function(){
+            try{
+                var retValidateParamDataStatus = validateParamDataCorporateTieupRequest();
+                if(retValidateParamDataStatus===false){
+                    $rootScope.isShowCorporateTieupRequestErrorMsg = true;
+                    $rootScope.corporateTieupErrorMsgStr = 'Please fill-up form details to send request for corporate tieup !';
+                }else{
+                    // get param obj to add corporate tie-up request
+                    var preparedParamJsonObj = getParamDataObjForCorporateTieupRequest();
+                    // console.log("addCorporateTieupRequest preparedParamJsonObj=>"+JSON.stringify(preparedParamJsonObj));
+                    if(preparedParamJsonObj!==false && jQuery.isEmptyObject(preparedParamJsonObj)===false){
+                        var jsonParamBlockUIObject = {};
+                        jsonParamBlockUIObject['css'] = {"padding":10};
+                        jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
+                        showHideLoaderBox('show', jsonParamBlockUIObject);
+                        
+                        var fetchAreaParamJsonObj = {};
+                        fetchAreaParamJsonObj['dkParamDataArr'] = preparedParamJsonObj;
+
+                        // calling CorporateTieupServices to add corporate tieup request
+                        CorporateTieupServices.addingCorporateTieupRequest(fetchAreaParamJsonObj).done(function(retResponseJson){
+                            $scope.$apply(function(){
+                                showHideLoaderBox('hide');
+                                if(retResponseJson!==false && retResponseJson!==undefined && retResponseJson!==''){
+                                    // console.log("addPartyOrdersRequest retResponseJson=>"+JSON.stringify(retResponseJson));
+                                    var retStatus = extractDataFromReturnAjaxResponse('GET', 'apiFile', 'isCorporateTieupRequestSend', retResponseJson);
+                                    if(retStatus==='YES'){
+                                        clearCustomizeOrderRequestFormField();
+                                        $rootScope.isShowCorporateTieupRequestFormContent = false;
+                                        $rootScope.isShowCorporateTieupRequestSendThankyouMsg = true;
+                                    }else{
+                                        $rootScope.isShowCorporateTieupRequestErrorMsg = true;
+                                        $rootScope.corporateTieupErrorMsgStr = 'Please try again to send request for corporate tie-up !';
+                                    }
+                                }
+                            });
+                        });
+                    }
+                }
+            }catch(ex){
+                console.log("problem in addCorporateTieupRequest ex=>"+ex);
+                showHideLoaderBox('hide');
+            }
+        };
+    }catch(ex){
+        console.log("problem in corporate tieup controller ex=>"+ex);
+    }
 });
