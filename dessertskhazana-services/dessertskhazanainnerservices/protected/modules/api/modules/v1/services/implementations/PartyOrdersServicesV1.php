ac1bc59cf1e288274c54a373c9b1daa2859e6bf1
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-21 21:59:18 +0530

    code committed related to dk system data

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/PartyOrdersServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/PartyOrdersServicesV1.php
@@ -7,30 +7,28 @@
 
 class PartyOrdersServicesV1 implements IPartyOrdersServicesV1{
     
-    // CJ defined this action 2016-07-20
+    // CJ defined this action 2016-08-21
     public function addPartyOrdersRequest($dkParamDataArr){
         $rspDetails = array();
         $rspDetails["isPartyOrderRequestSend"] = 'NO';
-        // check requested param data
-        if(count($dkParamDataArr)>0 && $dkParamDataArr!='' && $dkParamDataArr!=false){
-            // fetch unmd5 user data 
-            $unmd5UserDataArr = UsersDao :: getUserDetails($dkParamDataArr);
-            if(count($unmd5UserDataArr)==1 && $unmd5UserDataArr!=false){
-                $dkParamDataArr['user_id'] = $unmd5UserDataArr[0]['unmd5UserId'];
-                $dkParamDataArr['created_by'] = $unmd5UserDataArr[0]['unmd5UserId'];
+        // check requested param data length
+        if(count($dkParamDataArr)>0 && $dkParamDataArr!=false){
+            // add party order request status
+            $dkParamDataArr['status_id'] = 'R';
+            // fetch user session data details
+            $userSessionDetailsData = commonfunction :: getUserSessionDetails($dkParamDataArr);
+            if(count($userSessionDetailsData)>0 && $userSessionDetailsData!=false){
+                $dkParamDataArr['user_id'] = $userSessionDetailsData['unmd5UserId'];
+                $dkParamDataArr['created_by'] = $userSessionDetailsData['unmd5UserId'];
             }
-            // add party order request
-            $dkParamDataArr['status_id'] = '1';
+            $dkParamDataArr['partyorder_no'] = commonfunction :: generatePartyOrderNo();
             $lastPORID = PartyOrdersDao::addPartyOrderRequest($dkParamDataArr);
             if($lastPORID>0){
                 $rspDetails["isPartyOrderRequestSend"] = 'YES';
-                // add request to log
-                $dkParamDataArr['party_id'] = $lastPORID;
-                $lastPORLogID = PartyOrdersDao::addPartyOrderRequestLog($dkParamDataArr);
                 // send sms to end user to inform about party order request recieve by desserts khazana
-                $retEmailSentStatus = commonfunction :: preparedDataSendingEmailAboutPartyOrdersRequestReceiveFromCustomer($dkParamDataArr);
-                $smsMsgBody = "Sms Testing CJ";
-                $retSmsSentStatus = utils :: sendSMS(array("9975967186"), $smsMsgBody);
+                // $retEmailSentStatus = commonfunction :: preparedDataSendingEmailAboutPartyOrdersRequestReceiveFromCustomer($dkParamDataArr);
+                // $smsMsgBody = "Sms Testing CJ";
+                // $retSmsSentStatus = utils :: sendSMS(array("9975967186"), $smsMsgBody);
             }   
         } 
         ComponentsJson::GenerateJsonAndSend($rspDetails);
