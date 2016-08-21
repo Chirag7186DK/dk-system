9d042f6d439c38f5ebb04e7a2aefc3d4c614ee2c
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-21 20:20:42 +0530

    code committed related to dk system data

--- js/2services/28_UsersServices.js
+++ js/2services/28_UsersServices.js
@@ -35,7 +35,7 @@ app.factory('UsersServices', function($http, $q, $rootScope){
                 $rootScope.loggedUserName = userDashboardDataObj['loggedUserName'];
                 $rootScope.userSinceFrom = userDashboardDataObj['userSinceFrom'];
                 $rootScope.isEnableRatingReviewSubmitButton = true;
-                $rootScope.userInfoSectionListArr = userDashboardDataObj['userInfoSectionListArr'];
+                $rootScope.userInfoSectionListArrObj = userDashboardDataObj['userInfoSectionListArr'];
                 if(userDashboardDataObj['isUserLoggedInSession']==='Y'){
                     $rootScope.isUserLoggedInSession = true;
                 }else{
@@ -46,7 +46,7 @@ app.factory('UsersServices', function($http, $q, $rootScope){
                 $rootScope.userSinceFrom = '';
                 $rootScope.isEnableRatingReviewSubmitButton = false;
                 $rootScope.isUserLoggedInSession = false;
-                $rootScope.userInfoSectionListArr = false;
+                $rootScope.userInfoSectionListArrObj = false;
             }
         };
         
