3af8eac7340366b7c8074e8232be892418a6831a
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-23 02:26:06 +0530

    code commited related to dksystem

--- js/2services/30_PartyOrderServices.js
+++ js/2services/30_PartyOrderServices.js
@@ -4,7 +4,11 @@ app.factory('PartyOrdersServices', function($http, $q){
     try{
         var partyOrdersDetails = {
             addPartyOrderRequest:function(preparedParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrdersRequest", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){});
+                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){});
+                return promiseObject;
+            },
+            getPartyOrdersList:function(preparedParamJsonObj){
+                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){});
                 return promiseObject;
             }
         };
