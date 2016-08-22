891594ca9efbba323f7c91d8293830aacb41f0bd
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-23 01:52:33 +0530

    code commited related to dksystem

--- partyorder.php
+++ partyorder.php
@@ -29,12 +29,14 @@
         
         <!-- header -->
         <div class="col-xm-12 col-sm-12 col-md-12 col-lg-12 fHeaderContainerDivClass {{stickNtStickWebAppHeaderClass}}">
+            
             <!-- web logo header --->
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 webAppLogoAndMenuIconContainerDivClass">
                 <h1 class='webLogoHClass'>
                     <img class='dkLogoImgClass' src="#" load-dklogo-images-directive>
                 </h1>
             </div>
+            
             <!-- top menu bar -->
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fHeader_topMenuBarContainerDivClass">
                 <ul class="topMenuBarULClass list-inline">
@@ -112,110 +114,6 @@
             
             <!-- create horizontally space div between -->
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 commonHorizontalSpaceDivClass"></div>
-        
-            <!-- party order form content -->
-            <div ng-show="isShowPartyOrderRequestFormContent" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_formContentWrapperDivClass">
-                <p class="po_formHeaderPClass">
-                    <i class="fa fa-smile-o po_smileIconClass"></i> Hey please fill-up this form and we will get back within one hours. Party orders request will be accept only for Pune city.
-                </p>
-                <div ng-controller="PartyOrdersController" ng-init="attachedFieldValidationPartyOrdersRequest()" class="col-xs-12 col-sm-12 col-md-6 col-lg-6 po_formContentWrappperContainerDivClass">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_contactPersonNameContainerDivClass">
-                        <p class="po_formfieldLabelPClass">
-                            <i class="po_formfieldiconclass fa fa-user faa-tada animated"></i> Type contact name
-                        </p>
-                        <input placeholder="Type contact name" autocomplete="on" type="text" id='po_contactPersonNameInputId' class="form-control po_contactPersonNameInputClass">
-                        <p class="po_formfieldHintPClass">
-                            Eg: Chirag D Jain
-                        </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_contactMobileContainerDivClass">
-                        <p class="po_formfieldLabelPClass">
-                            <i class="po_formfieldiconclass fa fa-phone faa-tada animated"></i> Type mobile
-                        </p>
-                        <input placeholder="Type mobile" autocomplete="on" type="text" id='po_contactMobileInputId' class="form-control po_contactMobileInputClass">
-                        <p class="po_formfieldHintPClass">
-                            Eg: 9975967186
-                        </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_contactEmailContainerDivClass">
-                        <p class="po_formfieldLabelPClass">
-                            <i class="po_formfieldiconclass fa fa-envelope faa-tada animated"></i> Type email-id
-                        </p>
-                        <input placeholder="Type email-id" autocomplete="on" type="text" id='po_contactEmailInputId' class="form-control po_contactEmailInputClass">
-                        <p class="po_formfieldHintPClass">
-                            Eg: cjain9975@gmail.com
-                        </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_occasionContainerDivClass">
-                        <p class="po_formfieldLabelPClass">
-                            <i class="po_formfieldiconclass fa fa-heart-o faa-tada animated"></i> What's the occasion ?
-                        </p>
-                        <input placeholder="What's the occasion" autocomplete="on" type="text" id='po_occasionTitleInputId' class="form-control po_occasionTitleInputClass">
-                        <p class="po_formfieldHintPClass">
-                            Eg: Brother Birthday celebration, Mom & Dad Anniversary celebration
-                        </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_peopleContainerDivClass">
-                        <p class="po_formfieldLabelPClass">
-                            <i class="po_formfieldiconclass fa fa-user faa-tada animated"></i> How many awesome people to treat ?
-                        </p>
-                        <input placeholder="How many awesome people to treat" autocomplete="on" type="text" id='po_nosPeopleInputId' class="form-control po_nosPeopleInputClass">
-                        <p class="po_formfieldHintPClass">
-                            Eg: 20
-                        </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_dateContainerDivClass">
-                        <p class="po_formfieldLabelPClass">
-                            <i class="po_formfieldiconclass fa fa-calendar faa-tada animated"></i> Party date (YYYY-MM-DD) ?
-                        </p>
-                        <input placeholder="Party date" autocomplete="on" type="text" id='po_dateInputId' class="form-control po_dateInputClass">
-                        <p class="po_formfieldHintPClass">
-                            Eg: <?php echo date('Y-m-d');?>
-                        </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_venueContainerDivClass">
-                        <p class="po_formfieldLabelPClass">
-                            <i class="po_formfieldiconclass fa fa-map-marker faa-tada animated"></i> Party venue ?
-                        </p>
-                        <textarea placeholder="Party venue" autocomplete="on" class='form-control po_venueInputClass' id='po_venueInputId' rows="5" cols="20"></textarea>
-                        <p class="po_formfieldHintPClass">
-                            Eg: 421302 Bhiwandi
-                        </p>
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_requirementsContainerDivClass">
-                        <p class="po_formfieldLabelPClass">
-                            <i class="po_formfieldiconclass fa fa-envelope faa-tada animated"></i> Enter Requirements
-                        </p>
-                        <textarea placeholder="Party requirements" autocomplete="on" class='form-control po_messageInputClass' id='po_messageInputId' rows="5" cols="20"></textarea>
-                        <p class="po_formfieldHintPClass">
-                            Eg: Cakes,Chocolates,Sweets,Ice-cream, birthday party etc...
-                        </p>
-                    </div>
-                    <div ng-show="isShowPartyOrderRequestErrorMsg" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_errorMsgContainerDivClass">
-                        {{partyOrderErrorMsgStr}}
-                    </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_btnContainerDivClass">
-                        <button ng-click="addPartyOrdersRequest()" class='btn partyOrderRequestSubmtBtnClass' id='partyOrderRequestSubmtBtnId'>
-                            SEND REQUEST
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <!-- thank you party order requested message content -->
-            <div ng-show="isShowPartyOrderRequestSendThankyouMsg" ng-controller="PartyOrdersController" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 po_thankyouContentWrapperDivClass">
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 po_thankyouContentWrapperContainerDivClass">
-                    <p class="po_thankyoubodyPClass">
-                        <i class="fa fa-smile-o po_thankyousmileIconClass"></i> 
-                        Hurrah! Your details have been submitted & request party order no: {{requestedPartyOrderNo}}
-                        Let’s begin the party order preparations.
-                    </p>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <button ng-click="redirectToViewPartyOrderRequest()" class='btn redirectToViewPartyOrderRequestBtnClass' id='partyOrderRequestSubmtBtnId'>
-                            Another Party Order Request
-                        </button>
-                    </div>
-                </div>
-            </div>
             
         </div>
         
