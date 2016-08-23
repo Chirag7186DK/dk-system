ec19d731c5e078223935941af114feed420eef49
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-24 01:49:32 +0530

    code commited related to dksystem

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/controllers/CustomizeOrdersController.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/controllers/CustomizeOrdersController.php
@@ -8,7 +8,7 @@
 class CustomizeOrdersController extends V1Controller{
     
     // CJ defined this action 2016-07-24
-    public function actionManageCustomizeOrdersRequest(){
+    public function actionManageCustomizeOrders(){
         if(ComponentsHttp::httpMethod()=="POST"){
             // checking requested param key name 
             $retRequestedParamKeyStatusFromInDtoFile = customparam :: checkRequestedParamKeyFromInDtoFile($this->_inDtoArray);
@@ -20,6 +20,25 @@ class CustomizeOrdersController extends V1Controller{
                 if($retParamDataCorrectIncorrectStatus=='TRUE'){
                     $CustomizeOrdersServicesV1 = new CustomizeOrdersServicesV1();
                     $CustomizeOrdersServicesV1->addCustomizeOrdersRequest($dkParamDataArr);
+                }else{
+                    commonfunction :: generateResponseDataForInvalidRequestParamKeyData();
+                }
+            }else{
+                commonfunction :: generateResponseDataForInvalidRequestParamKey();
+            }
+        }
+        if(ComponentsHttp::httpMethod()=="GET"){
+            // checking requested param key name 
+            $retRequestedParamKeyStatusFromInDtoFile = customparam :: checkRequestedParamKeyFromInDtoFile($this->_inDtoArray);
+            if($retRequestedParamKeyStatusFromInDtoFile!=false && $retRequestedParamKeyStatusFromInDtoFile!=''){
+                $inDtoArray = $this->_inDtoArray;
+                $dkParamDataArr = $inDtoArray['dkParamDataArr'];
+                // checking param key value data & return status
+                $retParamDataCorrectIncorrectStatus = customparam :: checkParamDataForAuthenticatedUserDetails($dkParamDataArr);
+                if($retParamDataCorrectIncorrectStatus=='TRUE'){
+                    $CustomizeOrdersServicesV1 = new CustomizeOrdersServicesV1();
+                    $rspDetails = $CustomizeOrdersServicesV1->getCustomizeOrderList($dkParamDataArr);
+                    ComponentsJson::GenerateJsonAndSend($rspDetails);
                 }else{
                     commonfunction :: generateResponseDataForInvalidRequestParamKeyData();
                 }
