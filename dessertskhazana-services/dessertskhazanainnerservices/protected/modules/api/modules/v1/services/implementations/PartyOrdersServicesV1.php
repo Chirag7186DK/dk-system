36e09e08be19ff26004fe4f4ff24be321310bf73
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-25 00:01:22 +0530

    code commited related to dialy attendance csv file uploading

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/PartyOrdersServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/PartyOrdersServicesV1.php
@@ -21,9 +21,6 @@ class PartyOrdersServicesV1 implements IPartyOrdersServicesV1{
             $userSessionDetailsData = commonfunction :: getUserSessionDetails($dkParamDataArr);
             if(count($userSessionDetailsData)>0 && $userSessionDetailsData!=false){
                 $dkParamDataArr['partyorder_no'] = commonfunction :: generatePartyOrderNo();
-                $dkParamDataArr['name'] = $userSessionDetailsData['userName'];
-                $dkParamDataArr['mobile'] = $userSessionDetailsData['userMobile'];
-                $dkParamDataArr['email'] = $userSessionDetailsData['userEmail'];
                 $dkParamDataArr['user_id'] = $userSessionDetailsData['unmd5UserId'];
                 $dkParamDataArr['created_by'] = $userSessionDetailsData['unmd5UserId'];
                 $lastPORID = PartyOrdersDao::addPartyOrderRequest($dkParamDataArr);
