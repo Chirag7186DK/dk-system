a6a59c9b012ddc254866b2224ed6e185dc585bb3
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-25 00:09:16 +0530

    code commited related to dialy attendance csv file uploading

--- js/2services/32_CustomizeOrderServices.js
+++ js/2services/32_CustomizeOrderServices.js
@@ -4,9 +4,13 @@ app.factory('CustomizeOrdersServices', function($http, $q){
     try{
         var customizeOrdersDetails = {
             addCustomizeOrderRequest:function(preparedParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrdersRequest", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){
+                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){
                     return retResponseJson;
                 });
+                return promiseObject;
+            },
+            getCustomizeOrdersList:function(preparedParamJsonObj){
+                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
                 return promiseObject;
             }
         };
