5c3915b13ad50d690f6fb432e879dc25077acaf6
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-27 01:45:05 +0530

    code commited related cigs report cart priting

--- dessertskhazana-services/dessertskhazanainnerservices/protected/dao/DiscountCouponDao.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/dao/DiscountCouponDao.php
@@ -39,15 +39,53 @@ class DiscountCouponDao{
     }
     
     // CJ defined this function 2016-08-26
-    public static function getUserSharingDiscountCouponList($userId){
+    public static function getUserDiscountCouponAvailableForSharing($userId){
         $result = false;
         try{
             $connection = Yii::App()->db;
-            $sql= "";
+            $sql= "SELECT
+                    dcg.id dcgId, dcg.code dcgCode, dcg.title dcgTitle,
+                    COALESCE(dcg.is_universally, 'N') isUniversallyAccepted,
+                    COALESCE(dcg.is_percentagebased, 'N') isPercentageBased,
+                    COALESCE(dcg.percentage_based, 0) percentageBased,
+                    COALESCE(dcg.is_cashback_based, 'N') isCashbackBased,
+                    COALESCE(dcg.cashback_based, 0) cashbackBased,
+                    COALESCE(dcg.above_orderamount, '') aboveOrderAmt,
+                    COALESCE((CASE WHEN dcg.for_userid IS NULL THEN 'N' ELSE 'Y' END), 'N') isDiscountCouponAvailableForLoggedUser,
+                    COALESCE(dcg.for_userid, '') userId
+                    FROM DK_DISCOUNTCOUPONGENERATION dcg
+                    WHERE 1
+                    AND dcg.status='A'
+                    AND CURDATE() BETWEEN dcg.start_datedtime AND dcg.end_datedtime
+                    AND dcg.for_userid='$userId'
+                    AND dcg.is_universally='N'
+                    AND dcg.can_shareit='Y'
+                    AND dcg.share_limit>0";
             $command = $connection->createCommand($sql);
-            $userSharingDiscountCouponListDetailsArr = $command->queryAll();
-            if(count($userSharingDiscountCouponListDetailsArr)>0 && $userSharingDiscountCouponListDetailsArr!=false){
-                $result =  $userSharingDiscountCouponListDetailsArr;
+            $userDiscountCouponAvailableForSharingDetailsArr = $command->queryAll();
+            if(count($userDiscountCouponAvailableForSharingDetailsArr)>0 && $userDiscountCouponAvailableForSharingDetailsArr!=false){
+                $result =  $userDiscountCouponAvailableForSharingDetailsArr;
+            }
+        }catch(Exception $ex){}
+        return $result;
+    }
+    
+    // CJ defined this function 2016-08-26
+    public static function getCountUserSharedDiscountCoupon($userId, $discountCouponId){
+        $result = false;
+        try{
+            $connection = Yii::App()->db;
+            $sql= "SELECT
+                    COALESCE(COUNT(*), 0) countUserSharedDiscountCoupon
+                    FROM DK_USER_SHARED_DISCOUNTCOUPON usdc 
+                    WHERE
+                    usdc.user_id='$userId'
+                    AND usdc.discount_couponid='$discountCouponId'
+                    AND usdc.status='S'";
+            $command = $connection->createCommand($sql);
+            $countUserSharedDiscountCouponDetailsArr = $command->queryAll();
+            if(count($countUserSharedDiscountCouponDetailsArr)>0 && $countUserSharedDiscountCouponDetailsArr!=false){
+                $result =  $countUserSharedDiscountCouponDetailsArr;
             }
         }catch(Exception $ex){}
         return $result;
