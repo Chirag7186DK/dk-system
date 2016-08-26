be7f66f30fd78f9ca7449df538cde043d764d72c
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-27 03:07:01 +0530

    code commited related cigs report cart priting

---
+++ js/2services/32_DiscountCouponServices.js
@@ -0,0 +1,19 @@
+
+// CJ defined this function 2016-06-16
+app.factory('DiscountCouponServices', function($http, $q, $rootScope){
+    try{
+        
+        var discountCouponDetails = {};
+        
+        // userSharingDiscountCouponList
+        discountCouponDetails.userSharingDiscountCouponList = function(preparedParamJsonObj){
+            var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/DiscountCoupon/UserSharingDiscountCouponList", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+            return promiseObject;
+        };
+        
+        return discountCouponDetails;
+        
+    }catch(ex){
+        return false;
+    }
+}); 
