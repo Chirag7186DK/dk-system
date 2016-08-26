f431ce5e817dbac64340d0035642d0456120f364
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-27 01:20:32 +0530

    code commited related cigs report cart priting

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/DiscountCouponServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/DiscountCouponServicesV1.php
@@ -6,4 +6,17 @@
 */
 
 class DiscountCouponServicesV1 implements IDiscountCouponServicesV1{
+
+    public function getUserSharingDiscountCouponList($dkParamDataArr){
+        $rspDetails = array();
+        // checking param data length
+        if(count($dkParamDataArr)>0 && $dkParamDataArr!=false){
+            // fetch user session data details
+            $userSessionDetailsData = commonfunction :: getUserSessionDetails($dkParamDataArr);
+            if(count($userSessionDetailsData)>0 && $userSessionDetailsData!=false){
+            }
+        } 
+        return $rspDetails;
+    }
+
 }
