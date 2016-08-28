d97c4cf94a8ad53a75813c502cc0e9c5a4d29cd6
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-28 12:46:43 +0530

    code comited related to dk-system & wl code remove temporary

--- dessertskhazana-services/dessertskhazanainnerservices/protected/dao/DiscountCouponDao.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/dao/DiscountCouponDao.php
@@ -7,6 +7,41 @@
 
 class DiscountCouponDao{
     
+    // CJ defined this function 2016-08-28
+    public static function getDiscountCouponSetupDetails($paramJson){
+        $retResult = false;
+        try{
+            $connection = Yii::App()->db;
+            $sql= "SELECT 
+                dcg.id dcgId,
+                dcg.code dcgCode, 
+                dcg.title dcgTitle,
+                COALESCE(dcg.is_universally, 'N') isUniversallyAccepted,
+                COALESCE(dcg.is_percentagebased, 'N') isPercentageBased,
+                COALESCE(dcg.percentage_based, 0) percentageBased,
+                COALESCE(dcg.is_cashback_based, 'N') isCashbackBased,
+                COALESCE(dcg.cashback_based, 0) cashbackBased,
+                COALESCE(dcg.above_orderamount, '') aboveOrderAmt,
+                COALESCE((CASE WHEN dcg.for_userid IS NULL THEN 'N' ELSE 'Y' END), 'N') isDiscountCouponAvailableForLoggedUser,
+                COALESCE(dcg.for_userid, '') userId
+                FROM DK_DISCOUNTCOUPONGENERATION dcg
+                WHERE 1
+                AND dcg.status='A'";
+                if(array_key_exists('discount_couponid', $paramJson)){
+                    if($paramJson['discount_couponid']!='' && ($paramJson['discount_couponid'])>0){
+                        $sql.=" AND dcg.id='".$paramJson['discount_couponid']."'";
+                    }
+                }
+            $command = $connection->createCommand($sql);
+            $discountCouponSetupDetailArr = $command->queryAll();
+            if(count($discountCouponSetupDetailArr)>0 && $discountCouponSetupDetailArr!=false){
+                $retResult =  $discountCouponSetupDetailArr;
+            }
+        }catch(Exception $ex){}
+        return $retResult;
+    }
+    
+    
     // CJ defined this function 2016-08-13
     public static function getDiscountCouponAvailableForUser($userId=''){
         $result = false;
