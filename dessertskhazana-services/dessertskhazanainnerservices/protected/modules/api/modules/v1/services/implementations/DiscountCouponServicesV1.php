34cbb5ede10deae1ab11905f6c772ff731864443
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-28 13:20:46 +0530

    code comited related to dk-system & wl code remove temporary

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/DiscountCouponServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/DiscountCouponServicesV1.php
@@ -52,17 +52,54 @@ class DiscountCouponServicesV1 implements IDiscountCouponServicesV1{
             // fetch user session data details
             $userSessionDetailsData = commonfunction :: getUserSessionDetails($dkParamDataArr);
             if(count($userSessionDetailsData)>0 && $userSessionDetailsData!=false){
-                $unMd5UserId = $userSessionDetailsData['unmd5UserId'];
-                $dkParamDataArr['user_sessionid'] = $dkParamDataArr['user_sessionid'];
-                $dkParamDataArr['sharedby_id'] = $unMd5UserId;
-                $dkParamDataArr['created_by'] = $unMd5UserId;
-                $dkParamDataArr['created_datedtime'] = date('Y-m-d H:i:s');
-                $dkParamDataArr['status'] = 'S';
-                // storing tracking shared offers data for other users
-                $offerSharedDataStatus = DiscountCouponDao :: addUserSharedDiscountCoupon($dkParamDataArr);
-                
-                
-                
+                // fetch given discount coupon id setup details
+                $fetchSDCouponParamData =  array();
+                $fetchSDCouponParamData['discount_couponid'] = $dkParamDataArr['discount_couponid'];
+                $discountCouponSetupDetailsArr = DiscountCouponDao :: getDiscountCouponSetupDetails($fetchSDCouponParamData);
+                if(count($discountCouponSetupDetailsArr)==1 && $discountCouponSetupDetailsArr!=false){
+                    $unMd5UserId = $userSessionDetailsData['unmd5UserId'];
+                    $dkParamDataArr['user_sessionid'] = $dkParamDataArr['user_sessionid'];
+                    $dkParamDataArr['sharedby_id'] = $unMd5UserId;
+                    $dkParamDataArr['created_by'] = $unMd5UserId;
+                    $dkParamDataArr['created_datedtime'] = date('Y-m-d H:i:s');
+                    $dkParamDataArr['status'] = 'S';
+                    // storing tracking shared offers data for other users
+                    $lastInsertedIdOfferShared = DiscountCouponDao :: addUserSharedDiscountCoupon($dkParamDataArr);
+                    if($lastInsertedIdOfferShared>0 && $lastInsertedIdOfferShared!=false){
+                        // deciding discount coupon valid end datetime what will be while sharing offers to other users
+                        $startDateObj = date_create(date('Y-m-d H:i:s'));
+                        $nosOfDays = $discountCouponSetupDetailsArr[0]['discountCouponValidNosDaysRemain'];
+                        $discountCouponValidEndDateTime = date_add($startDateObj, date_interval_create_from_date_string("$nosOfDays days"));
+                        
+                        $addSDCouponParamData =  array();
+                        $addSDCouponParamData['code'] = $discountCouponSetupDetailsArr[0]['dcgCode'];
+                        $addSDCouponParamData['title'] = $discountCouponSetupDetailsArr[0]['dcgTitle'];
+                        $addSDCouponParamData['is_universally'] = 'N';
+                        $addSDCouponParamData['is_percentagebased'] = $discountCouponSetupDetailsArr[0]['isPercentageBased'];
+                        $addSDCouponParamData['percentage_based'] = $discountCouponSetupDetailsArr[0]['percentageBased'];
+                        $addSDCouponParamData['is_cashback_based'] = $discountCouponSetupDetailsArr[0]['isCashbackBased'];
+                        $addSDCouponParamData['cashback_based'] = $discountCouponSetupDetailsArr[0]['cashbackBased'];
+                        $addSDCouponParamData['start_datedtime'] = date('Y-m-d H:i:s');
+                        $addSDCouponParamData['end_datedtime'] = $discountCouponValidEndDateTime;
+                        $addSDCouponParamData['above_orderamount'] = $discountCouponSetupDetailsArr[0]['aboveOrderAmt'];
+                        $addSDCouponParamData['limit_used'] = '1';
+                        $addSDCouponParamData['for_userid'] = '';
+                        $addSDCouponParamData['can_shareit'] = 'N';
+                        $addSDCouponParamData['share_limit'] = '0';
+                        $addSDCouponParamData['created_by'] = $unMd5UserId;
+                        $addSDCouponParamData['created_datedtime'] = date('Y-m-d H:i:s');
+                        $addSDCouponParamData['status'] = 'A';
+                        // storing discount coupon setup details
+                        $lastInsertedIdSetupDiscountCoupon = DiscountCouponDao :: addSetupDiscountCouponGeneration($addSDCouponParamData);
+                        if($lastInsertedIdSetupDiscountCoupon>0 && $lastInsertedIdSetupDiscountCoupon!=false){
+                            $rspDetails['isOffersShared'] = 'TRUE';
+                            // send sms to end user to inform about offers has been shared by his/her friends/colleagues
+                            // $smsMsgBody = "Sms Testing CJ";
+                            // $sharedOfferUsermobile = '9975967186';
+                            // $retSmsSentStatus = utils :: sendSMS(array($sharedOfferUsermobile), $smsMsgBody);
+                        }
+                    }
+                }
             }
         } 
         return $rspDetails;
