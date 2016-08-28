ab57c3a5ed9a678be9a1d0dacb9e5438dd403fc2
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-28 11:23:10 +0530

    code comited related to dk-system & wl code remove temporary

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/controllers/DiscountCouponController.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/controllers/DiscountCouponController.php
@@ -53,4 +53,27 @@ class DiscountCouponController extends V1Controller{
         }
     }
     
+    // CJ defined this action 2016-08-28
+    public function actionSharingOffersFrmOneUserToOtherUser(){
+        
+        if(ComponentsHttp::httpMethod()=="POST"){
+            // checking requested param key name 
+            $retRequestedParamKeyStatusFromInDtoFile = customparam :: checkRequestedParamKeyFromInDtoFile($this->_inDtoArray);
+            if($retRequestedParamKeyStatusFromInDtoFile!=false && $retRequestedParamKeyStatusFromInDtoFile!=''){
+                $inDtoArray = $this->_inDtoArray;
+                $dkParamDataArr = $inDtoArray['dkParamDataArr'];
+                $retParamDataCorrectIncorrectStatus = customparam :: checkParamDataForAuthenticatedUserDetails($dkParamDataArr);
+                if($retParamDataCorrectIncorrectStatus=='TRUE'){
+                    $DiscountCouponServicesV1 = new DiscountCouponServicesV1();
+                    $rspDetails = $DiscountCouponServicesV1->getUserSharedDiscountCouponList($dkParamDataArr);
+                    ComponentsJson::GenerateJsonAndSend($rspDetails);
+                }else{
+                    commonfunction :: generateResponseDataForInvalidRequestParamKeyData();
+                }
+            }else{
+                commonfunction :: generateResponseDataForInvalidRequestParamKey();
+            }
+        }
+    }
+    
 }
