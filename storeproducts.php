cb2b0372b84ba0dd8b77dad67858b5f18d9a3fd7
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-28 21:01:04 +0530

    code comited related to dk-system & wl code remove temporary

--- storeproducts.php
+++ storeproducts.php
@@ -265,10 +265,6 @@ include "Site_config.inc.php";
                     <p class='cshopstore_productWeightPClass'>
                         Size : {{eachProductDetails.productFeatureDisplayMeasurementType}}
                     </p>
-                    <p class='cshopstore_productDeliveryLocationPClass'> 
-                        <!-- CJ need to think about product delivery location to show on screen -->
-                        Delivery <i class="fa fa-map-marker"></i> : At area 12
-                    </p>
                     <p class='cshopstore_productBtnWrapperPClass'> 
                         <button ng-controller='OrderCartController' ng-click="checkProductDataToAddInOrdercart(eachProductDetails, false, 'viewstoreproducts')" title='Click to add {{eachProductDetails.productListTitle}} item in order cart' class="cshopstore_specificProductAddBtnClass btn">
                             <i class="fa fa-plus"></i>
