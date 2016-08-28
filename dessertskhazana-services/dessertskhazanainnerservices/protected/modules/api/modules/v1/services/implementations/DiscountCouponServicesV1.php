4dd5ea566c728037810b2a68c2dff13fdfbc871d
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-28 09:12:26 +0530

    code comited related to dk-system & wl code remove temporary

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/DiscountCouponServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/DiscountCouponServicesV1.php
@@ -7,6 +7,7 @@
 
 class DiscountCouponServicesV1 implements IDiscountCouponServicesV1{
 
+    // CJ defined this service 2016-08-28
     public function getUserSharingDiscountCouponList($dkParamDataArr){
         $rspDetails = array();
         // checking param data length
@@ -18,6 +19,24 @@ class DiscountCouponServicesV1 implements IDiscountCouponServicesV1{
                 $retDataArr = commonfunction :: preparedDataToGetUserSharingDiscountCouponList($userId);
                 if(count($retDataArr)>0 && $retDataArr!=false){
                     $rspDetails['userSharingAllDiscountCouponList'] = $retDataArr;
+                }
+            }
+        } 
+        return $rspDetails;
+    }
+
+    // CJ defined this service 2016-08-28
+    public function getUserSharedDiscountCouponList($dkParamDataArr){
+        $rspDetails = array();
+        // checking param data length
+        if(count($dkParamDataArr)>0 && $dkParamDataArr!=false){
+            // fetch user session data details
+            $userSessionDetailsData = commonfunction :: getUserSessionDetails($dkParamDataArr);
+            if(count($userSessionDetailsData)>0 && $userSessionDetailsData!=false){
+                $userId = $userSessionDetailsData['unmd5UserId'];
+                $retDataArr = commonfunction :: preparedDataToGetUserSharedDiscountCouponList($userId);
+                if(count($retDataArr)>0 && $retDataArr!=false){
+                    $rspDetails['userSharedAllDiscountCouponList'] = $retDataArr;
                 }
             }
         } 
