4226fd063f738aed69cdfc71dd9e54a80f346a4c
Chirag Jain <chirag.jain@digitaledu.net>
2016-08-30 23:51:54 +0530

    code commited related to dksystem

--- js/controller/38_RefreshWebAppDataController.js
+++ js/controller/38_RefreshWebAppDataController.js
@@ -8,22 +8,6 @@ function RefreshWebAppDataController($scope, $rootScope, $http, UsersServices, O
             var userDashboardSummaryDataObj = UsersServices.refreshUserDashboardSummaryDataDetails();
             var userOrdercartDashboardSummaryDataObj = OrderCartServices.refreshUserOrdercartDashboardSummaryDataDetails();
             var addItemOrdercartDataOBj = OrderCartServices.addProductDataInOrdercartFromSession(false, false, 'session');
-            var infoObj = getInfoUserSelectedDeliveryCityAreaDessertsProductType();
-            if(infoObj!==false && infoObj!==undefined 
-                && infoObj!=='' && jQuery.isEmptyObject(infoObj)===false){
-                alert(JSON.stringify(infoObj));
-                var msgStr = "You are at : "+infoObj['userSelectedArea']+" - "+infoObj['userSelectedAreaPincode'];
-                var notifyInfoConfigObj = {
-                    icon:false,
-                    title:false,
-                    sound:false, 
-                    size:'normal', 
-                    msg:"<p style='text-align:center;'>"+msgStr+"</p>",
-                    delay:2000,
-                    position:"top left" 
-                }; 
-                showNotificationBoxMsg('', notifyInfoConfigObj);
-            }
         };
     }catch(ex){
         console.log("problem in RefreshWebAppDataController ex=>"+ex);
