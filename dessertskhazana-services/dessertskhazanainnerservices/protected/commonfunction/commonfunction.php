ff49f6c38964538573d7ea6ec6f8a252c1334876
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-02 21:12:20 +0530

    code commited related staff icard of stmiras college and removed addresss content

--- dessertskhazana-services/dessertskhazanainnerservices/protected/commonfunction/commonfunction.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/commonfunction/commonfunction.php
@@ -327,6 +327,52 @@ class commonfunction{
     
     
     
+    ////////////////./////////////// delivery city related code ////////////////////////////////
+    
+  
+    // CJ defined this function 2016-09-02
+    public static function getDeliveryCityListDetails($paramJsonData){
+        $rspDetails = array();
+        if(count($paramJsonData)>0 && $paramJsonData!=false){
+            $rsltJsonArr = array();
+            $rsltJsonArr['defaultSelectedDeliveryCityDetails'] = false;
+            $rsltJsonArr['allCityList'] = false;
+            // initial variable declare here
+            $gcity_ids = $paramJsonData['city_ids'];
+            $deliveryCityListDetailsArr = LocationDao::getCityList($gcity_ids, '');
+            if(count($deliveryCityListDetailsArr)>0 && $deliveryCityListDetailsArr!=false){
+                // iterate each delivery city details
+                $isRequestedDeliveryCityMatched = false;
+                for($eachIndex = 0; $eachIndex<count($deliveryCityListDetailsArr); $eachIndex++){
+                    $deliveryCityListDetailsArr[$eachIndex]['cityIcon'] = 'fa fa-map-marker';
+                    $deliveryCityListDetailsArr[$eachIndex]['isRequestedDeliveryCityMatched'] = 'N';
+                    if($deliveryCityListDetailsArr[$eachIndex]['cityId']==$gcity_ids){
+                        $isRequestedDeliveryCityMatched = true;
+                        $deliveryCityListDetailsArr[$eachIndex]['isRequestedDeliveryCityMatched'] = 'Y';
+                        // default selected delivery city to show
+                        $rsltJsonArr['defaultSelectedDeliveryCityDetails'] = array(
+                            "cityId"=>$gcity_ids,
+                            "cityName"=>$deliveryCityListDetailsArr[$eachIndex]['cityName'],
+                            "cityIcon"=>"fa fa-map-marker"
+                        );
+                    }
+                }
+                if($isRequestedDeliveryCityMatched==false){
+                    // default selected delivery city to show
+                    $rsltJsonArr['defaultSelectedDeliveryCityDetails'] = array(
+                        "cityId"=>$deliveryCityListDetailsArr[0]['cityId'],
+                        "cityName"=>$deliveryCityListDetailsArr[0]['cityName'],
+                        "cityIcon"=>"fa fa-map-marker"
+                    );
+                }
+                $rsltJsonArr['allCityList'] = $deliveryCityListDetailsArr;
+                $rspDetails["deliveryCityDetails"] =  $rsltJsonArr;
+            }
+        } 
+        return $rspDetails;
+    }
+ 
+    
     ////////////////.///////////////  user related code ////////////////////////////////
     
     
