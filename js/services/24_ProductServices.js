138f3d044f263bfe5fd6017ec109359f2fa9b29d
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-04 01:28:37 +0530

    code committed related to dk-system

--- js/services/24_ProductServices.js
+++ js/services/24_ProductServices.js
@@ -5,25 +5,37 @@ angular.module('DKAPP').factory('ProductServices', ProductServices);
 // CJ defined this function 2016-04-22
 function ProductServices($http, $q){
     try{
+        
         var productDetails = {};
         
-        productDetails.getProductTypeProductCategoryProductListForDashboardLevel = function(preparedParamJsonObj){
-            var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryProductListDashboardLevel", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+        productDetails.getProductTypeAllProductCategoryList = function(preparedParamJsonObj){
+            var jsonParamBlockUIObject = {};
+            jsonParamBlockUIObject['css'] = {"padding":10};
+            jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
+            showHideLoaderBox('show', jsonParamBlockUIObject);
+            var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeAllProductCategoryDetails", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){
+                showHideLoaderBox('hide');
+            });
             return promiseObject;
         };
+        
         productDetails.getProductTypeProductCategoryAllProductList = function(preparedParamJsonObj){
             var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryAllProductDetails", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
             return promiseObject;
         };
+        
         productDetails.getProductTypeProductCategoryProductDetails = function(preparedParamJsonObj){
             var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryProductDetails", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
             return promiseObject;
         };
+        
         productDetails.getProductDescriptionDetails = function(preparedParamJsonObj){
             var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductDescriptionDetails", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
             return promiseObject;
         };
+        
         return productDetails;
+        
     }catch(ex){
         console.log("problem in Product services ex=>"+ex);
         return false;
