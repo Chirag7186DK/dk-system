1c5babf6a9dffde83d9ded880d7b42fdcdc93500
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-04 01:29:09 +0530

    code committed related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/interfaces/IProductServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/interfaces/IProductServicesV1.php
@@ -6,6 +6,7 @@
 
 interface IProductServicesV1{
     public function getDeliveryAreaBasedDessertsTypeList($dessertsKhazanaInDtoArray);
+    public function getProductTypeAllProductCategoryDetails($dessertsKhazanaInDtoArray);
     public function getProductTypeProductCategoryAllProductDetails($dessertsKhazanaInDtoArray);
     public function getProductTypeProductCategoryProductDetails($dessertsKhazanaInDtoArray);
     public function getProductTypeProductCategoryProductDescriptionDetails($dessertsKhazanaInDtoArray);
