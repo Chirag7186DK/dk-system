f688b755c35edd0c853f2ad00b8a1b56b363933f
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-04 09:08:19 +0530

    code committed related to dk-system

--- js/services/24_ProductServices.js
+++ js/services/24_ProductServices.js
@@ -20,7 +20,13 @@ function ProductServices($http, $q){
         };
         
         productDetails.getProductTypeProductCategoryAllProductList = function(preparedParamJsonObj){
-            var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryAllProductDetails", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+            var jsonParamBlockUIObject = {};
+            jsonParamBlockUIObject['css'] = {"padding":10};
+            jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
+            showHideLoaderBox('show', jsonParamBlockUIObject);
+            var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryAllProductDetails", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){
+                showHideLoaderBox('hide');
+            });
             return promiseObject;
         };
         
