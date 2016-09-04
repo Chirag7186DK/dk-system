ecea8d5cec0d92efea54d210c671f4cfe7ae45a0
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-04 22:50:52 +0530

    code committed related to dk-system

--- all-store-cakes.php
+++ all-store-cakes.php
@@ -162,21 +162,24 @@
                 </div>
                 
             </div>
-
+            
+            <!-- all product category list will be loaded here -->
+            <div ng-show="productTypeAllProductCategoryList.length>1" ng-controller="ProductController" ng-init="loadProductTypeAllProductCategoryList()" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 cShopStoreProductAllCategoryWrapperDivClass">
+                <p ng-if="productTypeAllProductCategoryList.length>1" class='infoAbtProductCategoriesPClass'>
+                    Info *: <i class="fa fa-smile-o"></i> Hey you can choose products from 
+                    <span class="badge totalCountProductCategorySClass">
+                        {{productTypeAllProductCategoryList.length}}
+                    </span> categories !
+                </p>
+                <div ng-if="productTypeAllProductCategoryList.length>1" id='cShopStoreProductAllCategoryContainerDivId' class="col-xs-12 col-sm-12 col-md-12 col-lg-12 cShopStoreProductAllCategoryContainerDivClass">
+                    <li ng-click="storeDessertsTypeDataDetailsInSessionStorageToViewCStoreAllProductList(eachProductCategoryDetails)" ng-repeat="eachProductCategoryDetails in productTypeAllProductCategoryList | orderBy : '-isRequestedProductCategoryMatched'" title='Click here to view all product about {{eachProductCategoryDetails.productCategoryTitle}} desserts' class='cshopstore_eachProductCategoryContainerLIClass' scroll-horizontally-dessertsproducttype-productcategorylist-cshopstorelevel>
+                        {{eachProductCategoryDetails.productTypeProductCategoryTitle + '\n(' + eachProductCategoryDetails.totalProductCount + ')'}}
+                    </li>
+                </div>
+            </div>
+            
             <!-- all product list will be loaded here -->
             <div id='cShopStoreViewAllProductDetailsBodyWrapperDivId' ng-show="isLoadCShopStoreProductTypeProductCategoryAllProductList" ng-controller="ShopStoreController" ng-init="loadCShopStoreProductTypeProductCategoryAllProductList()" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 cShopStoreViewAllProductDetailsBodyWrapperDivClass">
-                
-                <!-- all product category list will be loaded here -->
-                <div ng-controller="ProductController" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 cShopStoreProductAllCategoryWrapperDivClass">
-                    <p class='infoAbtProductCategoriesPClass'>
-                        Info *: <i class="fa fa-smile-o"></i> Hey you are viewing '{{defaultSelectProductCategoryTitle}}' products from <span class="badge totalCountProductCategorySClass">{{productCategoryList.length}}</span> categories !
-                    </p>
-                    <div id='cShopStoreProductAllCategoryContainerDivId' class="col-xs-12 col-sm-12 col-md-12 col-lg-12 cShopStoreProductAllCategoryContainerDivClass">
-                        <li ng-click="collectDataToViewCShopstore(eachProductCategoryDetails)" ng-repeat="eachProductCategoryDetails in productCategoryList| orderBy : '-isRequestedProductCategoryMatched'" title='Click here to view all product about {{eachProductCategoryDetails.productCategoryTitle}} desserts' class='cshopstore_eachProductCategoryContainerLIClass' scroll-horizontally-dessertsproducttype-productcategorylist-cshopstorelevel>
-                            {{eachProductCategoryDetails.productTypeProductCategoryTitle + '\n(' + eachProductCategoryDetails.totalProductCount + ')'}}
-                        </li>
-                    </div>
-                </div>
                 
                 <!-- product filter operation main container div -->
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 cShopStoreProductFilterOperationWrapperDivClass">
