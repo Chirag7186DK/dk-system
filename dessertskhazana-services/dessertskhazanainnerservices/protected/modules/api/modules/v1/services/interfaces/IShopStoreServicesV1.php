29375ac9d4c22fa705ac4c7345f25303097207c8
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-05 01:19:40 +0530

    code committed related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/interfaces/IShopStoreServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/interfaces/IShopStoreServicesV1.php
@@ -7,7 +7,5 @@
 interface IShopStoreServicesV1{
     public function getDeliveryAreaBasedCStoreConductDessertType($dkInDtoArray);
     public function getCShopStoreSummaryInfo($dkInDtoArray);
-    public function getCShopStoreProductTypeProductCategoryAllProductDetails($dkInDtoArray);
     public function getCshopstoreWorkingstyleDetails($dkInDtoArray);
-    public function getCshopstoreProductdeliveryAreaInfo($dkInDtoArray);
 }
