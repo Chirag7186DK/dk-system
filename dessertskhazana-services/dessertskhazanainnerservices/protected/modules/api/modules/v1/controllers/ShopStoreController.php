869657eb10393b77053eb9f2245fefc44d108027
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-05 01:20:26 +0530

    code committed related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/controllers/ShopStoreController.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/controllers/ShopStoreController.php
@@ -53,28 +53,6 @@ class ShopStoreController extends V1Controller{
         }
     }
     
-    // CJ defined this action 2016-06-06
-    public function actionCShopStoreProductTypeProductCategoryAllProductDetails(){
-        if(ComponentsHttp::httpMethod()=="GET"){
-            // checking requested param key name 
-            $requestedParamKeyStatusFromInDtoFile = customparam :: checkRequestedParamKeyFromInDtoFile($this->_inDtoArray);
-            if($requestedParamKeyStatusFromInDtoFile!=false && $requestedParamKeyStatusFromInDtoFile!=''){
-                $inDtoArray = $this->_inDtoArray;
-                $dkParamDataArr = $inDtoArray['dkParamDataArr'];
-                // checking param key value data & return status
-                $paramDataCorrectIncorrectStatus = customparam :: checkParamDataFetchingCshopstoreAllProductDetails($dkParamDataArr);
-                if($paramDataCorrectIncorrectStatus=='TRUE'){
-                    $ShopStoreServicesV1 = new ShopStoreServicesV1();
-                    $ShopStoreServicesV1->getCShopStoreProductTypeProductCategoryAllProductDetails($dkParamDataArr);
-                }else{
-                    commonfunction :: generateResponseDataForInvalidRequestParamKeyData();
-                }
-            }else{
-                commonfunction :: generateResponseDataForInvalidRequestParamKey();
-            }
-        }
-    }
-    
     // CJ defined this action 2016-07-18
     public function actionCshopstoreWorkingstyle(){
         if(ComponentsHttp::httpMethod()=="GET"){
@@ -87,29 +65,8 @@ class ShopStoreController extends V1Controller{
                 $paramDataCorrectIncorrectStatus = customparam :: checkParamDataFetchingCshopstoreWorkingstyle($dkParamDataArr);
                 if($paramDataCorrectIncorrectStatus=='TRUE'){
                     $ShopStoreServicesV1 = new ShopStoreServicesV1();
-                    $ShopStoreServicesV1->getCshopstoreWorkingstyleDetails($dkParamDataArr);
-                }else{
-                    commonfunction :: generateResponseDataForInvalidRequestParamKeyData();
-                }
-            }else{
-                commonfunction :: generateResponseDataForInvalidRequestParamKey();
-            }
-        }
-    }
-    
-    // CJ defined this action 2016-07-18
-    public function actionCshopstoreProductdeliveryAreaInfo(){
-        if(ComponentsHttp::httpMethod()=="GET"){
-            // checking requested param key name 
-            $requestedParamKeyStatusFromInDtoFile = customparam :: checkRequestedParamKeyFromInDtoFile($this->_inDtoArray);
-            if($requestedParamKeyStatusFromInDtoFile!=false && $requestedParamKeyStatusFromInDtoFile!=''){
-                $inDtoArray = $this->_inDtoArray;
-                $dkParamDataArr = $inDtoArray['dkParamDataArr'];
-                // checking param key value data & return status
-                $paramDataCorrectIncorrectStatus = customparam :: checkParamDataFetchingCshopstoreProductDeliveryAreaInfo($dkParamDataArr);
-                if($paramDataCorrectIncorrectStatus=='TRUE'){
-                    $ShopStoreServicesV1 = new ShopStoreServicesV1();
-                    $ShopStoreServicesV1->getCshopstoreProductdeliveryAreaInfo($dkParamDataArr);
+                    $rspDetails = $ShopStoreServicesV1->getCshopstoreWorkingstyleDetails($dkParamDataArr);
+                    ComponentsJson::GenerateJsonAndSend($rspDetails);
                 }else{
                     commonfunction :: generateResponseDataForInvalidRequestParamKeyData();
                 }
