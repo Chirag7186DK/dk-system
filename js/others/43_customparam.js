a908bbedd0956966f07767b1118ee1abd1ec8d75
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-05 13:15:42 +0530

    code committed related to dk-system

--- js/others/43_customparam.js
+++ js/others/43_customparam.js
@@ -767,52 +767,6 @@ function getParamObjFromSessionForProductDescriptionDetails(){
 }
 
 // CJ defined this function 2016-07-10
-function checkAllParamToViewProductDetails(paramObj){
-    var retStatus = false;
-    try{
-        if(paramObj!==false && paramObj!=='' && jQuery.isEmptyObject(paramObj)===false){
-            if(paramObj.hasOwnProperty('shopStoreId')===true 
-                && paramObj.hasOwnProperty('shopStoreTitle')===true
-                && paramObj.hasOwnProperty('productTypeId')===true
-                && paramObj.hasOwnProperty('productTypeTitle')===true 
-                && paramObj.hasOwnProperty('productTypeProductCategoryId')===true 
-                && paramObj.hasOwnProperty('productTypeProductCategoryTitle')===true
-                && paramObj.hasOwnProperty('productListId')===true
-                && paramObj.hasOwnProperty('productListTitle')===true
-                && paramObj.hasOwnProperty('productFeatureId')===true ){
-                if(parseInt(paramObj['shopStoreId'])>0 && parseInt(paramObj['productTypeId'])>0
-                    && parseInt(paramObj['productTypeProductCategoryId'])>0
-                    && parseInt(paramObj['productListId'])>0 && parseInt(paramObj['productFeatureId'])>0
-                    && paramObj['shopStoreTitle']!=='' && paramObj['productTypeTitle']!==''
-                    && paramObj['productTypeProductCategoryTitle']!=='' && paramObj['productListTitle']!==''){
-                    
-                    // extract dk param obj && reset user product & dkSelectedDeliveryCityAreaDessertsProduct data
-                    var existingDkParamObj = $.parseJSON(sessionStorage.getItem('DKPARAMOBJ'));
-                    existingDkParamObj['dkSelectedDeliveryCityAreaDessertsProduct']['dessertsproduct'] = paramObj['productTypeId'];
-                    existingDkParamObj['dkSelectedDeliveryCityAreaDessertsProduct']['dessertsproducttitle'] = paramObj['productTypeTitle'];
-                    existingDkParamObj['userProduct']['shopstore_value'] = paramObj['shopStoreId'];
-                    existingDkParamObj['userProduct']['shopstore_name'] = paramObj['shopStoreTitle'];
-                    existingDkParamObj['userProduct']['producttype_value'] = paramObj['productTypeId'];
-                    existingDkParamObj['userProduct']['producttype_name'] =  paramObj['productTypeTitle'];
-                    existingDkParamObj['userProduct']['producttype_categoryvalue'] = paramObj['productTypeProductCategoryId'];
-                    existingDkParamObj['userProduct']['producttype_categoryname'] = paramObj['productTypeProductCategoryTitle'];
-                    existingDkParamObj['userProduct']['producttype_listvalue'] = paramObj['productListId'];
-                    existingDkParamObj['userProduct']['producttype_listname'] = paramObj['productListTitle'];
-                    existingDkParamObj['userProduct']['producttype_featurevalue'] = paramObj['productFeatureId'];
-                    existingDkParamObj['userProduct']['productviewed_bystatus'] = 'productwise';
-                    sessionStorage.setItem('DKPARAMOBJ', JSON.stringify(existingDkParamObj));
-                    retStatus = true;
-                }
-            }
-        }
-    }catch(ex){
-        retStatus = false;
-    }
-    return retStatus;
-}
-
-
-// CJ defined this function 2016-07-10
 function checkAllParamToViewCShopStore(paramObj){
     var retStatus = false;
     try{
