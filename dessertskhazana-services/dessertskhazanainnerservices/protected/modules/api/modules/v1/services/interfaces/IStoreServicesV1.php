17a30891b82ba1b5adcbba768894fcb06b462913
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-13 22:21:05 +0530

    code commited related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/interfaces/IStoreServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/interfaces/IStoreServicesV1.php
@@ -4,10 +4,10 @@
 * @author CJ defined this function 2016-06-24
 */
 
-interface IShopStoreServicesV1{
+interface IStoresServicesV1{
     public function getDeliveryAreaBasedDessertTypeStoresList($dkInDtoArray);
-    public function getDeliveryAreaBasedCStoreConductDessertType($dkInDtoArray);
-    public function getCShopStoreSummaryInfo($dkInDtoArray);
+    public function getDeliveryAreaBasedStoresConductDessertType($dkInDtoArray);
+    public function getStoreSummaryInfo($dkInDtoArray);
     public function getCshopstoreWorkingstyleDetails($dkInDtoArray);
     public function getStoreDeliveryFeeApplicableOnDeliveryArea($dkInDtoArray);
 }
