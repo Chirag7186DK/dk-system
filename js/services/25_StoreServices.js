a756eb6c0f11cee6f14c696d37fbca21cdc9a81b
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-13 22:43:59 +0530

    code commited related to dk-system

--- js/services/25_StoreServices.js
+++ js/services/25_StoreServices.js
@@ -29,7 +29,7 @@ function StoreServices($rootScope){
             return promiseObject;
         };
         
-        storeDetails.getCStoresSummaryInfo = function(preparedParamJsonObj){
+        storeDetails.getStoreSummaryInfo = function(preparedParamJsonObj){
             var jsonParamBlockUIObject = {};
             jsonParamBlockUIObject['css'] = {"padding":10};
             jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
