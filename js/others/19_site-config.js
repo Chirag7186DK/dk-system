37d4d34e1bfa44dd833b59e9fac1caff3cbae900
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-17 21:43:37 +0530

    code commited related to dk-system

---
+++ js/others/19_site-config.js
@@ -0,0 +1,14 @@
+
+isHeaderExecutingOnServerLevel = 'Y';
+apiUrlAccessHashValue = 'trr36pdthb9xbhcppyqkgbpkq';
+globalBaseSitePath = 'http://localhost/dk-system/';
+//globalBaseSitePath = 'http://192.168.1.103/dk-system/';
+product_versionyear = '2016';
+isProductInMaintainanceMode = 'N';
+isDisableDocumentRightClick = 'Y';
+
+// web-app loaded on devices type detect here width/height
+var deviceWidth = parseInt($(window).width());
+var deviceHeight = parseInt($(window).height());
+console.log("on load deviceWidth=>"+deviceWidth+", deviceHeight=>"+deviceHeight);
+//alert("on laod deviceWidth=>"+deviceWidth+", deviceHeight=>"+deviceHeight);
