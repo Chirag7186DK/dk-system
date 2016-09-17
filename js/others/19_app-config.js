a1017d693d8e99559394c38f3fbed168b4e52126
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-17 21:43:27 +0530

    code commited related to dk-system

---
+++ js/others/19_app-config.js
@@ -0,0 +1,16 @@
+
+// web-app module defined here 
+angular.module('DKAPP', []);
+
+//dkapp.config(['$stateProvider', '$urlRouterProvider',
+//    function ($stateProvider, $urlRouterProvider) {
+//        $stateProvider
+//            .state('', {
+//                templateUrl: ''
+//            })
+//            .state('', {
+//                templateUrl: ''
+//            });
+//        $urlRouterProvider.otherwise('/index.php');
+//    }
+//]);
