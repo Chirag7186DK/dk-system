565db61f7914a95c6461c7e9fcdbd9e89bd1b529
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-18 21:02:39 +0530

    code commited related to dk-system

--- js/services/27_ordercart-services.js
+++ js/services/27_ordercart-services.js
@@ -153,7 +153,19 @@
                 });
                 return promiseObject;
             };
-
+            
+            // ordercartItemList
+            orderDetails.getStorewiseOrderSummaryForCheckoutProcess = function(preparedParamJsonObj){
+                var jsonParamBlockUIObject = {};
+                jsonParamBlockUIObject['css'] = {"padding":10};
+                jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
+                showHideLoaderBox('show', jsonParamBlockUIObject);
+                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/OrderCart/StorewiseOrderSummaryCheckoutProcess", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){
+                    showHideLoaderBox('hide');
+                });
+                return promiseObject;
+            };
+            
             // resetAllItemOrdercart
             orderDetails.resetAllItemOrdercart = function(){
                 try{
