28375a493eb2a51b1de8051745f4c4c43809b37f
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-18 18:23:07 +0530

    code commited related to dk-system

--- js/services/32_customizeorder-services.js
+++ js/services/32_customizeorder-services.js
@@ -1,28 +1,32 @@
 
-angular.module('DKAPP').factory('CustomizeOrdersServices', CustomizeOrdersServices);
+(function(){
+    
+    angular.module('DKAPP').factory('CustomizeOrdersServices', CustomizeOrdersServices);
 
-// CJ defined this function 2016-07-24
-function CustomizeOrdersServices(){
-    try{
-        
-        var customizeOrdersDetails = {
-            
-            addCustomizeOrderRequest:function(preparedParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){
-                    return retResponseJson;
-                });
-                return promiseObject;
-            },
-            
-            getCustomizeOrdersList:function(preparedParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
-                return promiseObject;
-            }
-        };
-        
-        return customizeOrdersDetails;
-        
-    }catch(ex){
-        return false;
-    }
-}; 
+    // CJ defined this function 2016-07-24
+    function CustomizeOrdersServices(){
+        try{
+
+            var customizeOrdersDetails = {
+
+                addCustomizeOrderRequest:function(preparedParamJsonObj){
+                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){
+                        return retResponseJson;
+                    });
+                    return promiseObject;
+                },
+
+                getCustomizeOrdersList:function(preparedParamJsonObj){
+                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+                    return promiseObject;
+                }
+            };
+
+            return customizeOrdersDetails;
+
+        }catch(ex){
+            return false;
+        }
+    }; 
+    
+})();
