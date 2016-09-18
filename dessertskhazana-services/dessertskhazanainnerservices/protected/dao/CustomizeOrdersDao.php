b28018e645820cd4e0262b329af518fecef75151
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-18 18:49:17 +0530

    code commited related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/dao/CustomizeOrdersDao.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/dao/CustomizeOrdersDao.php
@@ -36,16 +36,18 @@ class CustomizeOrdersDao{
                 $sqlValues.="'".$coRequestParamDetails['customizeorder_no']."',";
             }
         }
-        if(array_key_exists('user_sessionid', $coRequestParamDetails)){
-            if($coRequestParamDetails['user_sessionid']!=''){
+        if(array_key_exists('user_sessionid', $paramDetails)){
+            if($paramDetails['user_sessionid']!='' 
+                && strlen($paramDetails['user_sessionid'])>=20){
                 $sqlColumnNames.=" user_sessionid,";
-                $sqlValues.="'".$coRequestParamDetails['user_sessionid']."',";
+                $sqlValues.="'".$paramDetails['user_sessionid']."',";
             }
         }
-        if(array_key_exists('user_id', $coRequestParamDetails)){
-            if($coRequestParamDetails['user_id']!=''){
+        if(array_key_exists('user_id', $paramDetails)){
+            if($paramDetails['user_id']!='' 
+                && ($paramDetails['user_id'])>0){
                 $sqlColumnNames.=" user_id,";
-                $sqlValues.="'".$coRequestParamDetails['user_id']."',";
+                $sqlValues.="'".$paramDetails['user_id']."',";
             }
         }
         if(array_key_exists('event_title', $coRequestParamDetails)){
@@ -54,10 +56,11 @@ class CustomizeOrdersDao{
                 $sqlValues.="'".$coRequestParamDetails['event_title']."',";
             }
         }
-        if(array_key_exists('nos_person', $coRequestParamDetails)){
-            if($coRequestParamDetails['nos_person']!=''){
+        if(array_key_exists('nos_person', $paramDetails)){
+            if($paramDetails['nos_person']!='' 
+                && ($paramDetails['nos_person'])>0){
                 $sqlColumnNames.=" nos_person,";
-                $sqlValues.="'".$coRequestParamDetails['nos_person']."',";
+                $sqlValues.="'".$paramDetails['nos_person']."',";
             }
         }
         if(array_key_exists('event_date', $coRequestParamDetails)){
@@ -84,23 +87,23 @@ class CustomizeOrdersDao{
                 $sqlValues.="'".$coRequestParamDetails['file']."',";
             }
         }
-        if(array_key_exists('created_by', $coRequestParamDetails)){
-            if($coRequestParamDetails['created_by']!=''){
+        if(array_key_exists('created_by', $paramDetails)){
+            if($paramDetails['created_by']!='' && ($paramDetails['created_by'])>0){
                 $sqlColumnNames.=" created_by,";
-                $sqlValues.="'".$coRequestParamDetails['created_by']."',";
+                $sqlValues.="'".$paramDetails['created_by']."',";
             }
         }
-        if(array_key_exists('status', $coRequestParamDetails)){
-            if($coRequestParamDetails['status']!=''){
+        if(array_key_exists('status', $paramDetails)){
+            if($paramDetails['status']!=''){
                 $sqlColumnNames.=" status,";
-                $sqlValues.="'".$coRequestParamDetails['status']."',";
+                $sqlValues.="'".$paramDetails['status']."',";
             }
         }
         if($sqlValues!='' && $sqlColumnNames!=''){
             $coRequestParamDetails['created_datedtime'] = date('Y-m-d H:i:s');
             $sqlColumnNames.=" created_datedtime,";
             $sqlValues.="'".$coRequestParamDetails['created_datedtime']."',";
-            $sqlQuery = " INSERT INTO DK_CUSTOMIZEORDERS_REQUEST " .rtrim("(".$sqlColumnNames, ',').") ".rtrim(" VALUES(".$sqlValues, ',').")";
+            $sqlQuery = " INSERT INTO CUSTOMIZEORDERS_REQUEST " .rtrim("(".$sqlColumnNames, ',').") ".rtrim(" VALUES(".$sqlValues, ',').")";
             $command = $connection->createCommand($sqlQuery);
             $result = $command->execute();
             if($result>=1){
@@ -128,23 +131,23 @@ class CustomizeOrdersDao{
                 $sqlValues.="'".$coRequestParamDetails['description']."',";
             }
         }
-        if(array_key_exists('status_id', $coRequestParamDetails)){
-            if($coRequestParamDetails['status_id']!=''){
-                $sqlColumnNames.=" status_id,";
-                $sqlValues.="'".$coRequestParamDetails['status_id']."',";
+        if(array_key_exists('created_by', $paramDetails)){
+            if($paramDetails['created_by']!=''){
+                $sqlColumnNames.=" created_by,";
+                $sqlValues.="'".$paramDetails['created_by']."',";
+                $paramDetails['created_datedtime'] = date('Y-m-d H:i:s');
+                $sqlColumnNames.=" created_datedtime,";
+                $sqlValues.="'".$paramDetails['created_datedtime']."',";
             }
         }
-        if(array_key_exists('created_by', $coRequestParamDetails)){
-            if($coRequestParamDetails['created_by']!=''){
-                $sqlColumnNames.=" created_by,";
-                $sqlValues.="'".$coRequestParamDetails['created_by']."',";
-                $coRequestParamDetails['created_datedtime'] = date('Y-m-d H:i:s');
-                $sqlColumnNames.=" created_datedtime,";
-                $sqlValues.="'".$coRequestParamDetails['created_datedtime']."',";
+        if(array_key_exists('status', $paramDetails)){
+            if($paramDetails['status']=='A' || $paramDetails['status']=='Z'){
+                $sqlColumnNames.=" status,";
+                $sqlValues.="'".$paramDetails['status']."',";
             }
         }
         if($sqlValues!='' && $sqlColumnNames!=''){
-            $sqlQuery = " INSERT INTO DK_CUSTOMIZRORDERS_REQUEST_LOG " .rtrim("(".$sqlColumnNames, ',').") ".rtrim(" VALUES(".$sqlValues, ',').")";
+            $sqlQuery = " INSERT INTO CUSTOMIZRORDERS_REQUEST_LOG " .rtrim("(".$sqlColumnNames, ',').") ".rtrim(" VALUES(".$sqlValues, ',').")";
             $command = $connection->createCommand($sqlQuery);
             $result = $command->execute();
             if($result>=1){
@@ -160,29 +163,30 @@ class CustomizeOrdersDao{
         try{
             $connection = Yii::App()->db;
             $sql= " SELECT
+                COALESCE(cor.id, '') customizeOrderId, 
                 COALESCE(cor.customizeorder_no, '') customizeOrderNo, 
-                COALESCE(cor.event_title, '') eventTitle,
+                COALESCE(cor.event_title, '') occassionTitle,
                 COALESCE(cor.nos_person, '') nosOfPerson, 
                 COALESCE(cor.event_date, '') eventDate, 
                 COALESCE(cor.event_venue, '') eventVenue, 
-                COALESCE(cor.event_requirements, '') customizeRequirements,
+                COALESCE(cor.event_requirements, '') eventRequirements,
+                COALESCE(cor.estimated_budget, 0) estimatedBudget,
+                COALESCE(cor.totalamount, 0) poTotalAmount,
                 (CASE 
-                    WHEN cor.status='R' THEN 'Requested'
-                    WHEN cor.status='CV' THEN 'Consulting with vendor'
-                    WHEN cor.status='C' THEN 'Confirmed by you & me'
+                    WHEN cor.status='R' THEN 'Requested by you'
+                    WHEN cor.status='CC' THEN 'Confirmed by you for further processing'
+                    WHEN cor.status='CV' THEN 'Consulting with our vendor'
                     WHEN cor.status='PP' THEN 'Payment Pending'
                     WHEN cor.status='PF' THEN 'Payment Failed'
-                    WHEN cor.status='PF' THEN 'Deleted by you'
-                    WHEN cor.status='ZA' THEN 'Deleted by us'
-                END) cortLongStatusMsg,
-                COALESCE(cor.status, '') corStatus,
-                '2000' estimatedAmt, '1800' confirmedAmt
-                FROM DK_USERS u 
-                JOIN DK_CUSTOMIZEORDERS_REQUEST cor ON cor.user_id=u.id
+                    WHEN cor.status='ZC' THEN 'Deleted/Removed by you'
+                    WHEN cor.status='ZA' THEN 'Deleted/Removed by us'
+                END) corLongStatusMsg, COALESCE(cor.status, '') corShortStatus
+                FROM USERS u 
+                JOIN CUSTOMIZEORDERS_REQUEST cor ON cor.user_id=u.id
                 WHERE 1
-                AND u.id='$userId'
-                AND cor.user_id='$userId'
-                AND u.status='A'";
+                AND u.status='A' 
+                AND u.id='$userId' AND cor.user_id='$userId'
+                ORDER BY cor.updated_datedtime DESC";
             $command = $connection->createCommand($sql);
             $customizeOrderDetailsArr = $command->queryAll();
             if(count($customizeOrderDetailsArr)>0 && $customizeOrderDetailsArr!=false){
