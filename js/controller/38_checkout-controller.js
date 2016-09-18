b7a42e52a0b15d85832bb2243fede61ed86a33f9
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-18 21:24:35 +0530

    code commited related to dk-system

--- js/controller/38_checkout-controller.js
+++ js/controller/38_checkout-controller.js
@@ -2,7 +2,7 @@
 angular.module('DKAPP').controller('CheckoutController', CheckoutController);
 
 // CheckoutController
-function CheckoutController($rootScope){
+function CheckoutController($rootScope, OrderCartServices){
     
     // gotoCheckout
     $rootScope.gotoCheckoutProcess = function(){
@@ -19,4 +19,37 @@ function CheckoutController($rootScope){
             console.log("problem in gotoCheckoutProcess ex=>"+ex);
         }
     };
+    
+    // loadStorewiseOrderSummaryForCheckoutProcess
+    $rootScope.loadStorewiseOrderSummaryForCheckoutProcess = function(){
+        try{
+            var authenticatedUserParamDataObj = getParamDataAuthenticatedUserDetailsFromSession();
+            if(authenticatedUserParamDataObj!==false 
+                && jQuery.isEmptyObject(authenticatedUserParamDataObj)===false){
+                var fetchedParamJsonObj = {};
+                fetchedParamJsonObj['dkParamDataArr'] = authenticatedUserParamDataObj;
+                // calling OrderCartServices 
+                OrderCartServices.getStorewiseOrderSummaryForCheckoutProcess(fetchedParamJsonObj).done(function(retResponseJson){
+                    $rootScope.$apply(function(){
+                        var storewiseOrderSummaryData =  false;
+                        if(retResponseJson!==false && retResponseJson!==undefined && retResponseJson!==''){
+                            storewiseOrderSummaryData = extractDataFromReturnAjaxResponse('GET', 'apiFile', 'storewiseOrderSummaryData', retResponseJson);
+                        }
+                        if(storewiseOrderSummaryData!==false && storewiseOrderSummaryData!==undefined 
+                            && jQuery.isEmptyObject(storewiseOrderSummaryData)===false){
+                            $rootScope.storewiseOrderSummaryData =  storewiseOrderSummaryData;
+                        }else{
+                            window.location.href = globalBaseSitePath;
+                        }
+                    });
+                });
+            }else{
+                window.location.href = globalBaseSitePath;
+            }
+        }catch(ex){
+            console.log("problem in loadStorewiseOrderSummaryForCheckoutProcess ex=>"+ex);
+        }
+    };
+
+    
 }
