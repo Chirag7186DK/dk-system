935b902a329bda6304749f23826216ca7587ff22
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-18 20:52:26 +0530

    code commited related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/interfaces/IOrderCartServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/interfaces/IOrderCartServicesV1.php
@@ -10,5 +10,6 @@ interface IOrderCartServicesV1{
     public function getOrdercartItemDetails($dkInDtoArray);
     public function updateItemDetailsInOrdercart($dkInDtoArray);
     public function removeItemDetailsFromOrdercart($dkInDtoArray);
+    public function getStorewiseOrderSummaryData($dkInDtoArray);
     public function resetAllItemOrdercart($dkInDtoArray);
 }
