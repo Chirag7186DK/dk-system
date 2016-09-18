465ccd7de35dcf311a8397ed7b690902b17717e9
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-18 21:13:43 +0530

    code commited related to dk-system

--- js/controller/38_checkout-controller.js
+++ js/controller/38_checkout-controller.js
@@ -4,4 +4,19 @@ angular.module('DKAPP').controller('CheckoutController', CheckoutController);
 // CheckoutController
 function CheckoutController($rootScope){
     
+    // gotoCheckout
+    $rootScope.gotoCheckoutProcess = function(){
+        try{
+            // check user logged in session
+            var authenticatedUserParamDataObj = getParamDataAuthenticatedUserDetailsFromSession();
+            if(authenticatedUserParamDataObj!==false 
+                && jQuery.isEmptyObject(authenticatedUserParamDataObj)===false){
+                window.location.href = globalBaseSitePath+"checkout.php";
+            }else{
+                window.location.href = globalBaseSitePath;
+            }
+        }catch(ex){
+            console.log("problem in gotoCheckoutProcess ex=>"+ex);
+        }
+    };
 }
