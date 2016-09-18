134a7b053d5425319c543b546fa0e0ddd799c042
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-18 13:58:50 +0530

    code commited related to dk-system

--- js/services/30_partyorder-services.js
+++ js/services/30_partyorder-services.js
@@ -1,21 +1,38 @@
 
-angular.module('DKAPP').factory('PartyOrdersServices', PartyOrdersServices);
+(function(){
 
-// CJ defined this function 2016-07-20
-function PartyOrdersServices($http, $q){
-    try{
-        var partyOrdersDetails = {
-            addPartyOrderRequest:function(preparedParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){});
-                return promiseObject;
-            },
-            getPartyOrdersList:function(preparedParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
-                return promiseObject;
-            }
-        };
-        return partyOrdersDetails;
-    }catch(ex){
-        return false;
-    }
-} 
+    angular.module('DKAPP').factory('PartyOrdersServices', PartyOrdersServices);
+
+    // CJ defined this function 2016-07-20
+    function PartyOrdersServices($http, $q){
+        try{
+            var partyOrdersDetails = {
+                addPartyOrderRequest:function(preparedParamJsonObj){
+                    var jsonParamBlockUIObject = {};
+                    jsonParamBlockUIObject['css'] = {"padding":10};
+                    jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
+                    showHideLoaderBox('show', jsonParamBlockUIObject);
+                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){
+                        showHideLoaderBox('hide');
+                    });
+                    return promiseObject;
+                },
+                getPartyOrdersList:function(preparedParamJsonObj){
+                    var jsonParamBlockUIObject = {};
+                    jsonParamBlockUIObject['css'] = {"padding":10};
+                    jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
+                    showHideLoaderBox('show', jsonParamBlockUIObject);
+                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){
+                        showHideLoaderBox('hide');
+                    });
+                    return promiseObject;
+                }
+            };
+            return partyOrdersDetails;
+        }catch(ex){
+            console.log("Problem in PartyOrdersServices=>"+ex);
+            return false;
+        }
+    } 
+    
+})();
