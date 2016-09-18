9dd80536d475c435fb56092c95c7cdb6b4ee42cc
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-18 18:24:11 +0530

    code commited related to dk-system

--- js/services/32_customizeorder-services.js
+++ js/services/32_customizeorder-services.js
@@ -10,14 +10,24 @@
             var customizeOrdersDetails = {
 
                 addCustomizeOrderRequest:function(preparedParamJsonObj){
+                    var jsonParamBlockUIObject = {};
+                    jsonParamBlockUIObject['css'] = {"padding":10};
+                    jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
+                    showHideLoaderBox('show', jsonParamBlockUIObject);
                     var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'POST', '', preparedParamJsonObj).done(function(retResponseJson){
-                        return retResponseJson;
+                        showHideLoaderBox('hide');
                     });
                     return promiseObject;
                 },
 
                 getCustomizeOrdersList:function(preparedParamJsonObj){
-                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){});
+                    var jsonParamBlockUIObject = {};
+                    jsonParamBlockUIObject['css'] = {"padding":10};
+                    jsonParamBlockUIObject['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
+                    showHideLoaderBox('show', jsonParamBlockUIObject);
+                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'GET', '', preparedParamJsonObj).done(function(retResponseJson){
+                        showHideLoaderBox('hide');
+                    });
                     return promiseObject;
                 }
             };
