b8717e105c135f580ccfaee0e1671e422e339d0e
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-19 22:21:56 +0530

    code commited related to class attendance sumamry report task and sql query changed

--- js/controller/37_ordercart-controller.js
+++ js/controller/37_ordercart-controller.js
@@ -3,6 +3,7 @@ angular.module('DKAPP').controller('OrderCartController', OrderCartController);
 
 // OrderCartController
 function OrderCartController($rootScope, OrderCartServices, StoreServices){
+    
     try{
          
         // checkProductDataToAddInOrdercart 
@@ -29,15 +30,14 @@ function OrderCartController($rootScope, OrderCartServices, StoreServices){
             }
         };
         
-        
         // addProductDataInOrdercart & item directly added in DB
         $rootScope.addProductDataInOrdercart = function(fcontentClass, productDataFromSession){
             try{
                 // collect product data
-                var preparedProductParamDataObj = getParamDataToAddProductInOrdercart(fcontentClass, productDataFromSession);
-                if(preparedProductParamDataObj!==false && jQuery.isEmptyObject(preparedProductParamDataObj)===false){
+                var paramDataObj = getParamDataToAddProductInOrdercart(fcontentClass, productDataFromSession);
+                if(paramDataObj!==false && jQuery.isEmptyObject(paramDataObj)===false){
                     var apiParamJsonObj = {};
-                    apiParamJsonObj['dkParamDataArr'] = preparedProductParamDataObj;
+                    apiParamJsonObj['dkParamDataArr'] = paramDataObj;
                     // calling OrderCartServices 
                     OrderCartServices.addItemOrdercart(apiParamJsonObj).done(function(retResponseJson){
                         $rootScope.$apply(function(){
@@ -64,6 +64,93 @@ function OrderCartController($rootScope, OrderCartServices, StoreServices){
                 console.log("problem in addProductDataInOrdercart ex=>"+ex);
             }
         };
+        
+        // gotoCheckoutProcess
+        $rootScope.gotoCheckoutProcess = function(){
+            try{
+                // check user logged in session
+                var authenticatedUserParamDataObj = getParamDataAuthenticatedUserDetailsFromSession();
+                if(authenticatedUserParamDataObj!==false 
+                    && jQuery.isEmptyObject(authenticatedUserParamDataObj)===false){
+                    window.location.href = globalBaseSitePath+"checkout.php";
+                }else{
+                    window.location.href = globalBaseSitePath;
+                }
+            }catch(ex){
+                console.log("problem in gotoCheckoutProcess ex=>"+ex);
+            }
+        };
+    
+        // loadStorewiseOrderSummaryForCheckoutProcess
+        $rootScope.loadStorewiseOrderSummaryForCheckoutProcess = function(){
+            try{
+                var paramDataObj = getParamDataAuthenticatedUserDetailsFromSession();
+                if(paramDataObj!==false && jQuery.isEmptyObject(paramDataObj)===false){
+                    var apiParamJsonObj = {};
+                    apiParamJsonObj['dkParamDataArr'] = paramDataObj;
+                    // calling OrderCartServices 
+                    OrderCartServices.getStorewiseOrderSummaryForCheckoutProcess(apiParamJsonObj).done(function(retResponseJson){
+                        $rootScope.$apply(function(){
+                            var allStorewiseOrderSummaryDataArrObj =  false;
+                            if(retResponseJson!==false && retResponseJson!==undefined && retResponseJson!==''){
+                                allStorewiseOrderSummaryDataArrObj = extractDataFromReturnAjaxResponse('GET', 'apiFile', 'storewiseOrderSummaryData', retResponseJson);
+                            }
+                            if(allStorewiseOrderSummaryDataArrObj!==false && allStorewiseOrderSummaryDataArrObj!==undefined 
+                                && jQuery.isEmptyObject(allStorewiseOrderSummaryDataArrObj)===false){
+                                $rootScope.allStorewiseOrderSummaryDataArrObj =  allStorewiseOrderSummaryDataArrObj;
+                            }else{
+                                window.location.href = globalBaseSitePath;
+                            }
+                        });
+                    });
+                }else{
+                    window.location.href = globalBaseSitePath;
+                }
+            }catch(ex){
+                console.log("problem in loadStorewiseOrderSummaryForCheckoutProcess ex=>"+ex);
+            }
+        };
+
+        //  checkOrderDeliveryAddressDataForSave
+        $rootScope.checkOrderDeliveryAddressDataForSave = function(fcontentClass){
+            // validating order delivery address details
+            var validatedDataStatus = validateOrderDeliveryAddressData(fcontentClass);
+            if(validatedDataStatus===true){
+                $rootScope.updateOrderDeliveryAddressInOrdercartStore(fcontentClass);
+            }else{
+                var notifyMsgStr = "Please enter delivery address !!!";
+                showNotificationBoxMsg(notifyMsgStr);
+            }
+        };
+    
+        // updateOrderDeliveryAddressInOrdercartStore
+        $rootScope.updateOrderDeliveryAddressInOrdercartStore = function(fcontentClass){
+            try{
+                // collect product data
+                var paramDataObj = getParamDataToAddOrderDeliveryAddressInOrdercartStore(fcontentClass);
+                if(paramDataObj!==false && jQuery.isEmptyObject(paramDataObj)===false){
+                    var apiParamJsonObj = {};
+                    apiParamJsonObj['dkParamDataArr'] = paramDataObj;
+                    // calling OrderCartServices 
+                    OrderCartServices.updateOrderDeliveryAddressInOrdercartStore(apiParamJsonObj).done(function(retResponseJson){
+                        $rootScope.$apply(function(){
+                            var isUpdatedOrderDeliveryAddress = 'FALSE';
+                            var notificationMsgStr = "Please try again to updated order delivery address !!!";
+                            if(retResponseJson!==false && retResponseJson!==undefined && retResponseJson!==''){
+                                isUpdatedOrderDeliveryAddress = extractDataFromReturnAjaxResponse('PUT', 'apiFile', 'isUpdatedOrderDeliveryAddress', retResponseJson);
+                            }
+                            if(isUpdatedOrderDeliveryAddress==='TRUE'){
+                                notificationMsgStr = "Order delivery address updated successfully !!!";
+                            }
+                            showNotificationBoxMsg(notificationMsgStr);
+                        });
+                    });
+                }
+            }catch(ex){
+                console.log("problem in updateOrderDeliveryAddressInOrdercartStore ex=>"+ex);
+            }
+        };
+
        
     }catch(ex){
         console.log("problem in OrdercartController ex=>"+ex);
