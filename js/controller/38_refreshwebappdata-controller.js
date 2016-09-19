2f3712748227cbd1d099d2f742423c0e32324c0f
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-19 21:59:04 +0530

    code commited related to class attendance sumamry report task and sql query changed

--- js/controller/38_refreshwebappdata-controller.js
+++ js/controller/38_refreshwebappdata-controller.js
@@ -1,13 +1,13 @@
 
 angular.module('DKAPP').controller('RefreshWebAppDataController', RefreshWebAppDataController);
 
-function RefreshWebAppDataController($scope, $rootScope, $http, UsersServices, OrderCartServices){
+function RefreshWebAppDataController($rootScope, UsersServices, OrderCartServices){
     try{
         // refreshWebAppData
         $rootScope.refreshWebAppData = function(){
-            var userDashboardSummaryDataObj = UsersServices.refreshUserDashboardSummaryDataDetails();
-            var userOrdercartDashboardSummaryDataObj = OrderCartServices.refreshUserOrdercartDashboardSummaryDataDetails();
-            var addItemOrdercartDataOBj = OrderCartServices.addProductDataInOrdercartFromSession(false, 'session');
+            UsersServices.refreshUserDashboardSummaryDataDetails();
+            OrderCartServices.refreshUserOrdercartDashboardSummaryDataDetails();
+            OrderCartServices.addProductDataInOrdercartFromSession(false, 'session');
         };
     }catch(ex){
         console.log("problem in RefreshWebAppDataController ex=>"+ex);
