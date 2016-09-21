a926d24f5d71cdbe1ca1684e379334360a9b15af
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-22 02:03:57 +0530

    code commited rleated to attendance bin task & overview attendance by teacherwise

--- account-signup-signin.php
+++ account-signup-signin.php
@@ -51,7 +51,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fHeaderRowBorderDivClass"></div>
 
         <!-- my-account body content-->
-        <div ng-controller="UsersController" ng-init="isEnableAccountSignUpSignIn()" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_bodyDivClass">
+        <div ng-controller="UsersController" ng-init="isUserAlreadySignedInAccountSection()" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_bodyDivClass">
             
             <!-- customer bread crumb -->
             <div customer-breadcrumb-directive class="col-xs-12 col-sm-12 col-md-12 col-lg-12 customerBreadcrumbDivClass">
@@ -69,11 +69,15 @@
                     </li>
                 </ul>
             </div>
-          
-            <!-- display customer sign-in form info -->
-            <div ng-hide="isUserLoggedInSession" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignInFormInfoWrapperDivClass">
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 ma_userSignInformContentWrappperContainerDivClass">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignInMobileContainerDivClass">
+            
+            <!-- create horizontally space div between -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 commonHorizontalSpaceDivClass"></div>
+        
+            <!-- display sign-in section info -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignInFormInfoWrapperDivClass">
+                <!-- display sign-in form info -->
+                <div ng-if="displaySignInSignUpSectionAccountName=='signIn'" class="col-xs-12 col-sm-12 col-sm-6 col-lg-6 col-md-offset-3 col-lg-offset-3 ma_userSignInformContentWrappperContainerDivClass">
+                    <div class="col-xs-12 col-sm-12 col-md-12 ma_userSignInEmailContainerDivClass">
                         <p class="ma_userSignInFormfieldLabelPClass">
                             <i class="ma_userSigInFormfieldiconclass fa fa-envelope faa-tada animated"></i> Email-Id
                         </p>
@@ -97,20 +101,114 @@
                         {{userSignInFormContentErrorMsgStr}}
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignInBtnContainerDivClass">
-                        <button ng-click="collectDataUserSignInAuthentication()" class='btn ma_userSigInSubmtBtnClass' id='ma_userSigInSubmtBtnId'>
-                            <i class='fa fa-key'></i> SIGN IN
+                        <button ng-click="collectDataUserSignInAuthentication()" class='btn ma_userSigInSubmtBtnClass'>
+                            <i class='fa fa-lock'></i> SIGN IN
                         </button>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignInBorderDivClass">
                         <span>New to Desserts Khazana ?</span>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_redirectForUserSignUpBtnContainerDivClass">
-                        <button ng-click="toggleWishListUserSignUpFormContent()" class='btn ma_redirectForUserSignUpBtnClass' id='ma_redirectForUserSignUpBtnId'>
-                            Signup for Desserts Khazana account
+                        <button ng-click="resetSignUpSignInAccountSection('signUp')" class='btn ma_redirectForUserSignUpBtnClass'>
+                            Signup with Desserts Khazana
                         </button>
                     </div>
                 </div>
             </div>
+            
+            <!-- display sign-up info section -->
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpFormInfoWrapperDivClass">
+                <!-- sign-up form info -->
+                <div ng-if="displaySignInSignUpSectionAccountName=='signUp'" class="col-xs-12 col-sm-12 col-sm-6 col-lg-6 col-md-offset-3 col-lg-offset-3 ma_userSignUpformContentWrappperContainerDivClass">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpNameContainerDivClass">
+                        <p class="ma_userSignUpFormfieldLabelPClass">
+                            <i class="ma_userSignUpFormfieldiconclass fa fa-user faa-tada animated"></i> Name
+                        </p>
+                        <input attach-validation-nameinput-directive type="text" id='ma_userSignUpNameInputId' class="form-control ma_userSignUpNameInputName">
+                        <p class="ma_userSignUpFormfieldHintPClass">
+                            Eg: Chirag Jain, Mahesh Gupta, Rahul G
+                        </p>
+                        <p class="ma_userSignUpNameInput_ErrorClass"></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpEmailContainerDivClass">
+                        <p class="ma_userSignUpFormfieldLabelPClass">
+                            <i class="ma_userSignUpFormfieldiconclass fa fa-envelope faa-tada animated"></i> Email-Id
+                        </p>
+                        <input type="text" id='ma_userSignUpEmailInputId' class="form-control ma_userSignUpEmailInputClass">
+                        <p class="ma_userSignUpFormfieldHintPClass">
+                            Eg: chirag.jain@gmail.com
+                        </p>
+                        <p class="ma_userSignUpEmailInput_ErrorClass"></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpMobileContainerDivClass">
+                        <p class="ma_userSignUpFormfieldLabelPClass">
+                            <i class="ma_userSignUpFormfieldiconclass fa fa-mobile faa-tada animated"></i> Mobile
+                        </p>
+                        <input attachvalidation-mobileinput-directive type="text" id='ma_userSignUpMobileInputId' class="form-control ma_userSignUpMobileInputClass">
+                        <p class="ma_userSignUpFormfieldHintPClass">
+                            Eg: 9975967186
+                        </p>
+                        <p class="ma_userSignUpMobileInput_ErrorClass"></p>
+                    </div>
+                    <div ng-show="isShowUserSignUpFormContentErrorMsg" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignInErrorMsgContainerDivClass">
+                        {{userSignUpFormContentErrorMsgStr}}
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpBtnContainerDivClass">
+                        <button ng-click="collectDataUserSignUpAuthentication()" class='btn ma_userSignUpSubmitBtnClass' id='ma_userSignUpSubmitBtnId'>
+                            <i class='fa fa-lock'></i> SIGN UP
+                        </button>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpBorderDivClass">
+                        <span>Already have an account ?</span>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_redirectForUserSignUpBtnContainerDivClass">
+                        <button ng-click="resetSignUpSignInAccountSection('signIn')" class='btn ma_redirectForUserSignInBtnClass' id='ma_redirectForUserSignInBtnId'>
+                            Sigin with Desserts Khazana
+                        </button>
+                    </div>
+                </div>
+                
+                <!-- sign-up text code form info -->
+                <div ng-if="displaySignInSignUpSectionAccountName=='otpCode'" class="col-xs-12 col-sm-12 col-sm-6 col-lg-6 col-md-offset-3 col-lg-offset-3 ma_userSignUpformContentWrappperContainerDivClass">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpOtpCodeContainerDivClass">
+                        <p class="ma_userSignUpFormfieldLabelPClass">
+                            <i class="ma_userSignUpFormfieldiconclass fa fa-mobile faa-tada animated"></i> OTP Code
+                        </p>
+                        <input type="text" id='ma_userSignUpOtpCodeInputId' class="form-control ma_userSignUpOtpCodeInputClass">
+                        <p class="ma_userSignUpFormfieldHintPClass">
+                            Eg: 23Sddd
+                        </p>
+                        <p class="ma_userSignUpOtpCodeInput_ErrorClass"></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpPasswordContainerDivClass">
+                        <p class="ma_userSignUpFormfieldLabelPClass">
+                            <i class="ma_userSignUpFormfieldiconclass fa fa-key faa-tada animated"></i> Password
+                        </p>
+                        <input type="password" id='ma_userSignUpPasswordInputId' class="form-control ma_userSignUpPasswordInputClass">
+                        <p class="ma_userSignUpFormfieldHintPClass">
+                            Eg: 23Sddd
+                        </p>
+                        <p class="ma_userSignUpPasswordInput_ErrorClass"></p>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpBtnContainerDivClass">
+                        <button ng-click="collectDataUserSignUpAuthentication()" class='btn ma_userSignUpSubmitBtnClass'>
+                            <i class='fa fa-lock'></i> SIGN UP
+                        </button>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_userSignUpBorderDivClass">
+                        <span>Already have an account ?</span>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ma_redirectForUserSignUpBtnContainerDivClass">
+                        <button ng-click="resetSignUpSignInAccountSection('signIn')" class='btn ma_redirectForUserSignInBtnClass'>
+                            Sigin with Desserts Khazana
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+
+            
+            
         </div>    
         
         <!-- logged authenticated user web application data refresh -->
