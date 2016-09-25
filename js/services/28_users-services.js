decb41ea9fdd8023dc39dc3502a60a597f445db3
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-25 09:22:51 +0530

    code commited related to dk-system

--- js/services/28_users-services.js
+++ js/services/28_users-services.js
@@ -35,13 +35,7 @@
             
             // sendOtpcode
             userDetails.sendOtpcode = function(paramDataObj){
-                var blockUIObj = {};
-                blockUIObj['css'] = {"padding":10};
-                blockUIObj['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
-                showHideLoaderBox('show', blockUIObj);
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Users/SendOtpCode", 'apiFile', 'POST', '', paramDataObj).done(function(retResponseJson){
-                    showHideLoaderBox('hide');
-                });
+                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Users/SendOtpCode", 'apiFile', 'POST', '', paramDataObj).done(function(retResponseJson){});
                 return promiseObject;
             };
 
