7ebcfb8b45fc0c536f98a1fb798b8ce45cfffeef
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-25 11:21:26 +0530

    code commited related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/customparam/customparam.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/customparam/customparam.php
@@ -692,12 +692,14 @@ class customparam{
         }
         if(array_key_exists('isRequestCheckingCreditional', $paramJsonData)
             && array_key_exists('isRequestValidateOtp', $paramJsonData)
+            && array_key_exists('tokenId', $paramJsonData)    
             && array_key_exists('otpcode', $paramJsonData)   
-            && array_key_exists('tokenId', $paramJsonData)       
+            && array_key_exists('otpcodeTokenId', $paramJsonData)       
             && array_key_exists('isRequestUpdatePwd', $paramJsonData)){
             if($paramJsonData['isRequestCheckingCreditional']=='N'
                 && $paramJsonData['isRequestValidateOtp']=='Y'
                 && strlen($paramJsonData['otpcode'])==6
+                && $paramJsonData['otpcodeTokenId']>0 && $paramJsonData['otpcodeTokenId']!=''
                 && $paramJsonData['tokenId']>0 && $paramJsonData['tokenId']!=''  
                 && $paramJsonData['isRequestUpdatePwd']=='N'){
                 $correctParamKeyValueDataCount++;
