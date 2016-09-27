271ed5bf2187ab03520ffeaec2bda22a5269cbe6
Chirag Jain <chirag.jain@digitaledu.net>
2016-09-28 01:12:36 +0530

    code commited rleated to validationn

--- dessertskhazana-services/dessertskhazanainnerservices/protected/validation/validation.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/validation/validation.php
@@ -5,8 +5,8 @@ class validation{
     
     public static function isNumberic($contentData){
         $rtStatus = 'FALSE';
-        if($contentData!='' && $contentData!=false && $contentData!=true){
-            $regPattern = "/^[1-9][0-9]{0,15}$/";
+        if($contentData!='' && $contentData!=false){
+            $regPattern = "/^[1-9]+$/";
             if(preg_match($regPattern, $contentData)>0){
                 $rtStatus = 'TRUE';
             }
