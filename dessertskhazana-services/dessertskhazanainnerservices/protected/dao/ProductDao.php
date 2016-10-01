d666a89f5049f4359b15b1e2153bd34df310d2cf
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-01 09:48:19 +0530

    code commited related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/dao/ProductDao.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/dao/ProductDao.php
@@ -51,7 +51,7 @@ class ProductDao{
                     COALESCE(UPPER(pt.name), '') productTypeTitleInCaps,
                     COALESCE(ppc.id, '') productTypeProductCategoryId, COALESCE(spac.name, '') productTypeProductCategoryTitle,
                     COALESCE(spl.id, '') productListId, COALESCE(spl.name, '') productListTitle,
-                    COALESCE(spl.deliverytime, '90 MIN') individualProductDeliveryTime, COALESCE(spl.availablesize, '1') productAvailableSizeCount,
+                    COALESCE(spl.deliverytime, '70 MIN') individualProductDeliveryTime, COALESCE(spl.availablesize, '1') productAvailableSizeCount,
                     COALESCE(splld.id, '') productFeatureId, COALESCE(splld.food_type, '') productFeatureFoodType, 
                     COALESCE(splld.taste_type, '') productFeatureTasteType, COALESCE(splld.pattern_type, '') productFeaturePatternType, 
                     COALESCE(splld.display_measurementtype, '') productFeatureDisplayMeasurementType,
