fc8ce5ca581d34c5667e8d707b67b5672df1d239
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 11:20:44 +0530

    code commited related to cigs report card task

--- js/others/19_app-config.js
+++ js/others/19_app-config.js
@@ -1,16 +1,19 @@
 
+'use strict';
+
 // web-app module defined here 
 var DKAPP = angular.module('DKAPP', ['ui.router']);
 
-DKAPP.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $stateProvider, $urlRouterProvider) {
+DKAPP.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', 
+    function($locationProvider, $stateProvider, $urlRouterProvider){
             
-    $stateProvider
-        .state('/', {
-            url:"/",
-            templateUrl: 'initial.php'
-        });
+        $stateProvider
+            .state('/', {
+                url:"/",
+                templateUrl: 'initial.php'
+            });
 
-    $urlRouterProvider.otherwise('/');
-    $locationProvider.html5Mode({ enabled: true, requireBase: true });
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode({ enabled: true, requireBase: true });
 
 }]);
