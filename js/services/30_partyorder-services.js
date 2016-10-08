9e1b1cfa16d64dbdb0ba066ea7266541005e3bc4
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 22:41:40 +0530

    code commited related to dk-system

--- js/services/30_partyorder-services.js
+++ js/services/30_partyorder-services.js
@@ -9,23 +9,13 @@
             var partyOrdersDetails = {
                 
                 addPartyOrderRequest:function(apiParamJsonObj){
-                    var blockUIObj = {};
-                    blockUIObj['css'] = {"padding":10};
-                    blockUIObj['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
-                    showHideLoaderBox('show', blockUIObj);
                     var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'POST', '', apiParamJsonObj).done(function(retResponseJson){
-                        showHideLoaderBox('hide');
                     });
                     return promiseObject;
                 },
                 
                 getPartyOrdersList:function(apiParamJsonObj){
-                    var blockUIObj = {};
-                    blockUIObj['css'] = {"padding":10};
-                    blockUIObj['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
-                    showHideLoaderBox('show', blockUIObj);
                     var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                        showHideLoaderBox('hide');
                     });
                     return promiseObject;
                 }
