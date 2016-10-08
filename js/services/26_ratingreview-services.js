c202c92017b892c72acd5e128d700cb6deb71e4e
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 22:37:43 +0530

    code commited related to dk-system

--- js/services/26_ratingreview-services.js
+++ js/services/26_ratingreview-services.js
@@ -4,55 +4,43 @@
     angular.module('DKAPP').factory('RatingReviewServices', RatingReviewServices);
 
     // CJ defined this function 2016-04-22
-    function RatingReviewServices(LoaderServices){
+    function RatingReviewServices(){
         try{
 
             var ratingReviewDetails = {};
 
             ratingReviewDetails.addUserRatingReviewProduct = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/UserRatingReviewProduct", 'apiFile', 'POST', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             ratingReviewDetails.getStoreRatingReviewQuestions = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/StoreRatingReviewQuestions", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             ratingReviewDetails.getStoreAllUserRatingReviewed = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/StoreAllUserRating", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             ratingReviewDetails.getAllUserRatingReviewProduct = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/AllUserRatingReviewProduct", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             ratingReviewDetails.getMaxRatingReviewProduct = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/MaxRatingReviewProduct", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             ratingReviewDetails.getAverageRatingReviewProduct = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/AverageRatingReviewProduct", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
