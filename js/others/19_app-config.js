f8ec3fe4cbbf4b4eeb10f4d53c29215ae73e0b09
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 11:49:06 +0530

    code commited related to cigs report card task

--- js/others/19_app-config.js
+++ js/others/19_app-config.js
@@ -8,12 +8,20 @@ DKAPP.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
     function($locationProvider, $stateProvider, $urlRouterProvider){
             
         $stateProvider
+            .state('all-stores-cakes', {
+                url:"/all-stores-cakes",
+                templateUrl: 'all-stores-cakes.php'
+            })
+            .state('home', {
+                url:"/home",
+                templateUrl: 'home.php'
+            })
             .state('/', {
                 url:"/",
-                templateUrl: 'initial.php'
+                templateUrl: 'home.php'
             });
 
         $urlRouterProvider.otherwise('/');
-        $locationProvider.html5Mode({ enabled: true, requireBase: true });
+        $locationProvider.html5Mode({ enabled: true, requireBase: true});
 
 }]);
