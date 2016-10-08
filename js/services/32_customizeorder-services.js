6ad8f8bcc8cc802fda008cafb2e66fbf002f3dbf
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 22:42:06 +0530

    code commited related to dk-system

--- js/services/32_customizeorder-services.js
+++ js/services/32_customizeorder-services.js
@@ -10,23 +10,13 @@
             var customizeOrdersDetails = {
 
                 addCustomizeOrderRequest:function(apiParamJsonObj){
-                    var blockUIObj = {};
-                    blockUIObj['css'] = {"padding":10};
-                    blockUIObj['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
-                    showHideLoaderBox('show', blockUIObj);
                     var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'POST', '', apiParamJsonObj).done(function(retResponseJson){
-                        showHideLoaderBox('hide');
                     });
                     return promiseObject;
                 },
 
                 getCustomizeOrdersList:function(apiParamJsonObj){
-                    var blockUIObj = {};
-                    blockUIObj['css'] = {"padding":10};
-                    blockUIObj['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana is loading........</center>";
-                    showHideLoaderBox('show', blockUIObj);
                     var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                        showHideLoaderBox('hide');
                     });
                     return promiseObject;
                 }
