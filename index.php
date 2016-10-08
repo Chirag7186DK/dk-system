56ba93822e8c2d3245fd3c985fd7a6a266af7823
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 12:56:54 +0530

    code commited related to dk-system

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <title><?php echo $SiteTitle;?></title>
+        <title>
+            <?php echo $SiteTitle; ?>
+        </title>
         <link rel="shortcut icon" href="images/dk/dklogo/fevicon.ico">
         <base href="/dk-system/" />
         <style>
@@ -21,12 +23,52 @@
     </head>
 
     <!-- body start here -->
-    <body ng-cloak class="ng-cloak">
+    <body ng-cloak class="ng-cloak" id="dkAppBodyId" id="dkAppBodyClass">
         
-        <!-- view will be loaded here-->
+        <!-- web logo header --->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 webAppLogoAndMenuIconContainerDivClass">
+            <h1 class='webLogoHClass'>
+                <img class='dkLogoImgClass' src="#" load-dklogo-images-directive>
+            </h1>
+        </div>
+        
+        <!-- top menu bar -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fHeader_topMenuBarContainerDivClass">
+            <ul class="topMenuBarULClass list-inline">
+                <li>
+                    <i class="fa fa-comment-o fa-flip-horizontal"></i> Care
+                </li>
+                <li ng-if="isShowSelectedDeliveryAreaTextHeader==true" class="selectedDeliveryAreaTextHeaderLIClass">
+                    <i class="fa fa-map-marker"></i> At: {{selectedDeliveryAreaTextHeader}}
+                </li>
+                <li ng-controller="UsersController" ng-click="ordercartItemRequestedCount>0 && storeRequestedSectionNameToAccessInUserAccount('ordercart');">
+                    <i class="fa fa-shopping-basket"></i> (Item: {{ordercartItemRequestedCount}}, Rs: {{subtotalOrderAmt}})
+                </li>
+                <li ng-if='isUserLoggedInSession==false' ng-click="goToSignUpSignInAccountSection('home')" ng-controller="UsersController">
+                    <i class="fa fa-user accountSignUpSignInIconClass"></i> Log In / Sign Up
+                </li>
+                <li ng-if='isUserLoggedInSession==true' ng-controller="UsersController" ng-click="toggleUserAccountSectionDropdown('home')">
+                    <i class="fa fa-user accountSignUpSignInIconClass"></i> {{loggedUserName}}
+                    <!-- user account showing different section label to access -->
+                    <div class="userAccountAllSectionListDropdownDivClass" ng-if='isUserLoggedInSession==true'>
+                        <div ng-repeat="userInfoEachSectionListArrObj in userInfoAllSectionListArrObj" title="{{userInfoEachSectionListArrObj.hoverTitle}}" class='userAccountEachSectionListDropdownDivClass' ng-click="storeRequestedSectionNameToAccessInUserAccount(userInfoEachSectionListArrObj.sectionName);">
+                            {{userInfoEachSectionListArrObj.displayTitle}}
+                        </div>
+                    </div>
+                </li>
+                <li ng-if='isUserLoggedInSession==true' ng-click="signOutUser()" ng-controller="UsersController">
+                    <i class="fa fa-sign-out"></i> Log Out
+                </li>
+            </ul>
+        </div>
+        
+        <!-- header row border div class -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fHeaderRowBorderDivClass"></div>
+        
+        <!-- dynamically view will be loaded here-->
         <div ui-view></div>
           
-        <!-- load all css & js file-->
+        <!-- first time will be load all css & js file -->
         <?php 
             include "loadAllJsCssFile.php"; 
         ?>
