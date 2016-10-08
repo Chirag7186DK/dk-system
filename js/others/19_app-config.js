d21658e365b1421fdcad4e2307831fdb40c74cfd
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 12:43:48 +0530

    code commited related to dk-system

--- js/others/19_app-config.js
+++ js/others/19_app-config.js
@@ -8,19 +8,35 @@ DKAPP.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
     function($locationProvider, $stateProvider, $urlRouterProvider){
             
         $stateProvider
-            .state('all-stores-cakes', {
-                url:"/all-stores-cakes",
-                templateUrl: 'all-stores-cakes.php'
+            .state('/', {
+                url:"/",
+                templateUrl: 'home.php'
             })
             .state('home', {
                 url:"/home",
                 templateUrl: 'home.php'
             })
-            .state('/', {
-                url:"/",
-                templateUrl: 'home.php'
+            .state('all-stores-cakes', {
+                url:"/all-stores-cakes",
+                templateUrl: 'all-stores-cakes.php'
+            })
+            .state('store-all-cakes', {
+                url:"/store-all-cakes",
+                templateUrl: 'store-all-cakes.php'
+            })
+            .state('cakes-product', {
+                url:"/cakes-product",
+                templateUrl: 'cakes-product.php'
+            })
+            .state('account-signup-signin', {
+                url:"/account-signup-signin",
+                templateUrl: 'account-signup-signin.php'
+            })
+            .state('customer-account', {
+                url:"/customer-account",
+                templateUrl: 'customer-account.php'
             });
-
+            
         $urlRouterProvider.otherwise('/');
         $locationProvider.html5Mode({ enabled: true, requireBase: true});
 
