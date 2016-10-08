6d5ffadd9e93aa6d87e5be9b5c79cec58ffe8024
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 22:36:41 +0530

    code commited related to dk-system

--- js/services/24_product-services.js
+++ js/services/24_product-services.js
@@ -4,47 +4,37 @@
     angular.module('DKAPP').factory('ProductServices', ProductServices);
 
     // CJ defined this function 2016-04-22
-    function ProductServices(LoaderServices){
+    function ProductServices(){
         try{
 
             var productDetails = {};
 
             productDetails.getProductTypeAllProductCategoryList = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeAllProductCategoryDetails", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             productDetails.getProductTypeProductCategoryFilterTypeList = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryFilterTypeList", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             productDetails.getProductTypeProductCategoryAllProductList = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryAllProductDetails", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             productDetails.getProductTypeProductCategoryProductDetails = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryProductDetails", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             productDetails.getProductDescriptionDetails = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductDescriptionDetails", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
