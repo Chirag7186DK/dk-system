604b5ffe6cd27782bae5ff017bf4713ee7b1d614
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 22:21:26 +0530

    code commited related to dk-system

--- js/services/32_loader-services.js
+++ js/services/32_loader-services.js
@@ -3,8 +3,24 @@
     
     angular.module('DKAPP').factory('LoaderServices', LoaderServices);
 
-    function LoaderServices($rootScope){
+    function LoaderServices(){
         try{
+            
+            var loaderDetails = {};
+            
+            loaderDetails.showLoader = function(){
+                var blockUIObj = {};
+                blockUIObj['css'] = {"padding":10, "border":"0px!important;"};
+                blockUIObj['message'] = "<img src='"+globalBaseSitePath+"images/loading.gif'><br><center>Please wait desserts khazana data is loading........</center>";
+                showHideLoaderBox('show', blockUIObj);
+            };
+            
+            loaderDetails.hideLoader = function(){
+                showHideLoaderBox('hide');
+            };
+            
+            return loaderDetails;
+            
         }catch(ex){
             console.log("problem in LocationServices ex=>"+ex);
             return false;
