4eb5a4cac4689efae8321ade8dce8afea823ef2a
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 12:26:29 +0530

    code commited related to dk-system

--- home.php
+++ home.php
@@ -1,51 +1,6 @@
 
 <div ng-controller='dkSessionController' ng-init="loadDefaultDataInDkSession('home');">
     
-    <!-- header -->
-    <div class="col-xm-12 col-sm-12 col-md-12 col-lg-12 fHeaderContainerDivClass {{stickNtStickWebAppHeaderClass}}">
-
-        <!-- web logo header --->
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 webAppLogoAndMenuIconContainerDivClass">
-            <h1 class='webLogoHClass'>
-                <img class='dkLogoImgClass' src="#" load-dklogo-images-directive>
-            </h1>
-        </div>
-
-        <!-- top menu bar -->
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fHeader_topMenuBarContainerDivClass">
-            <ul class="topMenuBarULClass list-inline">
-                <li title="Click here to contact us">
-                    <i class="fa fa-comment-o fa-flip-horizontal"></i> Care
-                </li>
-                <li ng-if="isShowSelectedDeliveryAreaTextHeader==true" class="selectedDeliveryAreaTextHeaderLIClass" title="Click here to change delivery location">
-                    <i class="fa fa-map-marker"></i> At: {{selectedDeliveryAreaTextHeader}}
-                </li>
-                <li ng-controller="UsersController" title="Click here to view your current order cart all item(s)" ng-click="ordercartItemRequestedCount>0 && storeRequestedSectionNameToAccessInUserAccount('ordercart');">
-                    <i class="fa fa-shopping-basket"></i> (Item: {{ordercartItemRequestedCount}}, Rs: {{subtotalOrderAmt}})
-                </li>
-                <li ng-if='isUserLoggedInSession==false' ng-click="goToSignUpSignInAccountSection('home')" ng-controller="UsersController" title="Click here to Log In / Sign Up with desserts khazana account">
-                    <i class="fa fa-user accountSignUpSignInIconClass"></i> Log In / Sign Up
-                </li>
-                <li ng-if='isUserLoggedInSession==true' ng-controller="UsersController" ng-click="toggleUserAccountSectionDropdown('home')">
-                    <i class="fa fa-user accountSignUpSignInIconClass"></i> {{loggedUserName}}
-                    <!-- user account showing different section label to access -->
-                    <div class="userAccountAllSectionListDropdownDivClass" ng-if='isUserLoggedInSession==true'>
-                        <div ng-repeat="userInfoEachSectionListArrObj in userInfoAllSectionListArrObj" title="{{userInfoEachSectionListArrObj.hoverTitle}}" class='userAccountEachSectionListDropdownDivClass' ng-click="storeRequestedSectionNameToAccessInUserAccount(userInfoEachSectionListArrObj.sectionName);">
-                            {{userInfoEachSectionListArrObj.displayTitle}}
-                        </div>
-                    </div>
-                </li>
-                <li ng-if='isUserLoggedInSession==true' ng-click="signOutUser()" ng-controller="UsersController" title="Click here to log out from desserts khazana account">
-                    <i class="fa fa-sign-out"></i> Log Out
-                </li>
-            </ul>
-        </div>
-
-    </div>
-
-    <!-- header row border div class -->
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fHeaderRowBorderDivClass"></div>
-
     <!-- delivery city area desserts product -->
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 deliveryCityAreaDessertsContainerDivClass">
 
@@ -133,13 +88,6 @@
                 </p>
             </div>
         </div>
-    </div>
-
-    <!-- how its works -->
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 howItsWorkContainerDivClass">
-        <p class='howItsWorkTitlePClass'>
-            <span>How it's works ?</span>
-        </p>
     </div>
 
     <!-- refresh web application data -->
