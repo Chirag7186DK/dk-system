05c0eff1d9ff3f6e4c43d4338257ccdfe946ebb4
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 17:20:02 +0530

    code commited related to dk-system

--- customizeorder.php
+++ customizeorder.php
@@ -1,136 +1,53 @@
 
-<?php 
-    include "Site_config.inc.php"; 
-?>
+<div ng-controller='dkSessionController' ng-init="loadDefaultDataInDkSession('customize-order');">
 
-<!DOCTYPE html>
-<html lang="en" ng-app='DKAPP'>
-    
-    <!-- head section start here -->
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <title><?php echo $SiteTitle;?></title>
-        <link rel="shortcut icon" href="images/dk/dklogo/fevicon.ico">
-        <style>
-            [ng\:cloak], [ng-cloak], [data-ng-cloak], [x-ng-cloak], .ng-cloak, .x-ng-cloak{
-                display:none!important;
-            }
-        </style>
-    </head>
+    <!-- customize order  -->
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 co_bodyDivClass">
 
-    <!-- body start here -->
-    <body ng-cloak scroll-window-directive resize-window-directive class="ng-cloak onBodyScrollClass" ng-controller='dkSessionController' ng-init="loadDefaultDataInDkSession('home'); checkInBackgroundDataAvailableForUpdationFromSession('customizeorder');">
-        
-        <!-- common SCROLL TOP BUTTON -->
-        <a class="scrollToTopBtnClass" ng-show="isShowScrollToTopBtnWebAppPage" href="#" title='Click to scroll up page'>
-            <i class="fa fa-angle-up"></i>
-        </a>
-        
-        <!-- first header -->
-        <div class="col-xm-12 col-sm-12 col-md-12 col-lg-12 fHeaderContainerDivClass {{stickNtStickWebAppHeaderClass}}">
-            
-            <!-- web logo header --->
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 webAppLogoAndMenuIconContainerDivClass">
-                <h1 class='webLogoHClass'>
-                    <img class='dkLogoImgClass' src="#" load-dklogo-images-directive>
-                </h1>
-            </div>
-            
-            <!-- top menu bar -->
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fHeader_topMenuBarContainerDivClass">
-                <ul class="topMenuBarULClass list-inline">
-                    <li title="Click here to contact us">
-                        <i class="fa fa-comment-o fa-flip-horizontal"></i> Care
-                    </li>
-                    <li ng-controller="UsersController" title="Click here to view your current order cart all item(s)" ng-click="ordercartItemRequestedCount>0 && storeRequestedSectionNameToAccessInUserAccount('ordercart');">
-                        <i class="fa fa-shopping-basket"></i> (Item: {{ordercartItemRequestedCount}}, Rs: {{subtotalOrderAmt}})
-                    </li>
-                    <li ng-if='isUserLoggedInSession==false' ng-click="redirectToAccountSignUpSignIn('home')" ng-controller="UsersController" title="Click here to Log In / Sign Up with desserts khazana account">
-                        <i class="fa fa-user accountSignUpSignInIconClass"></i> Log In / Sign Up
-                    </li>
-                    <li ng-if='isUserLoggedInSession==true' ng-controller="UsersController" ng-click="toggleUserAccountSectionDropdown('home')">
-                        <i class="fa fa-user accountSignUpSignInIconClass"></i> {{loggedUserName}}
-                        <!-- user account showing different section label to access -->
-                        <div class="userAccountAllSectionListDropdownDivClass" ng-if='isUserLoggedInSession==true'>
-                            <div ng-repeat="userInfoEachSectionListArrObj in userInfoAllSectionListArrObj" title="{{userInfoEachSectionListArrObj.hoverTitle}}" class='userAccountEachSectionListDropdownDivClass' ng-click="storeRequestedSectionNameToAccessInUserAccount(userInfoEachSectionListArrObj.sectionName);">
-                                {{userInfoEachSectionListArrObj.displayTitle}}
-                            </div>
-                        </div>
-                    </li>
-                    <li ng-if='isUserLoggedInSession==true' ng-click="signOutUser()" ng-controller="UsersController" title="Click here to log out from desserts khazana account">
-                        <i class="fa fa-sign-out"></i> Log Out
-                    </li>
-                </ul>
-            </div>
-            
-            <!-- show customer delivery city area desserts product type on header as text -->
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 showCustomerDeliveryCityAreaDessertsProductTypeTextForHeaderDivClass">
-                <p ng-show='isShowCustomerDeliveryCityAreaDessertsProductTypeTextForHeader' class="showCustomerDeliveryCityAreaDessertsProductTypeTextForHeaderPClass">
-                    <i class="fa fa-map-marker faa faa-tada animated showCustomerDeliveryCityAreaDessertsProductTypeTextIconClass"></i> {{customerDeliveryCityAreaDessertsProductTypeTextForHeader}}
-                </p>
-            </div>
-            
+        <!-- customer order bread crumb -->
+        <ul class="co_userBreadcrumbULClass list-inline">
+            <li class='co_userBreadcrumbHomeTitleLIClass'>
+                <a ui-sref="/">
+                    Home
+                </a>
+            </li>
+            <li class='customerBreadcrumbLIClass'>
+                |
+            </li>
+            <li class='co_userBreadcrumbCustomizeTitleLIClass'>
+                Customize Order
+            </li>
+        </ul>
+
+        <!-- customize order video -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 co_videoCoverageContainerDivClass">
+           Customize order video will be come here with full width
         </div>
-        
-        <!-- header row border div class -->
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 fHeaderRowBorderDivClass"></div>
-        
-        <!-- customize order  -->
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 co_bodyDivClass">
-            
-            <!-- customer order bread crumb -->
-            <div customer-breadcrumb-directive class="col-xs-12 col-sm-12 col-md-12 col-lg-12 co_userBreadcrumbDivClass">
-                <ul class="co_userBreadcrumbULClass list-inline">
-                    <li class='co_userBreadcrumbHomeTitleLIClass'>
-                        <a href="<?php echo $BaseSitePath;?>">
-                            {{customerBreadCrumbOnWebApp.homeKey}}
-                        </a>
-                    </li>
-                    <li class='co_userBreadcrumbCustomizeTitleLIClass'>
-                        |&nbsp; <a href="#">{{customerBreadCrumbOnWebApp.customizeOrderTitle}}</a>
-                    </li>
-                </ul>
-            </div>
-            
-            <!-- customize order video -->
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 co_videoCoverageContainerDivClass">
-               Customize order video will be come here with full width
-            </div>
-            
-            <!-- customize order content -->
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 co_contentContainerDivClass">
-                Customize order content will be come here
-            </div>
-            
-            <!-- if not logged user then sign-up/sign-in button -->
-            <div ng-if="isUserLoggedInSession==false" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 coSignInSignUpContainerDivClass">
-                <p class="coSignInSignUpInfoPClass">
-                    <i class="fa fa-user accountSignUpSignInIconClass"></i> 
-                    Please Log In / Sign Up to request for customize order
-                </p>
-                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 coSignInSignUpBtnDivClass">
-                    <button ng-click="redirectToAccountSignUpSignIn('customizeorder')" class='btn coSignInSignUpBtnClass' id='coSignInSignUpBtnId'>
-                        Log In / Sign Up
-                    </button>
-                </div>
-            </div>
-            
-            <!-- create horizontally space div between -->
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 commonHorizontalSpaceDivClass"></div>
-            
+
+        <!-- customize order content -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 co_contentContainerDivClass">
+            Customize order content will be come here
         </div>
-        
-        <!-- refresh web application data -->
-        <div id='refreshUserWebAppDataDivId' ng-controller="RefreshWebAppDataController" ng-init="refreshWebAppData()" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"></div>
-        
-        <!-- load all css & js file-->
-        <?php 
-            include "loadAllJsCssFile.php"; 
-        ?>
-        
-    </body>
-    
-</html>
 
+        <!-- if not logged user then sign-up/sign-in button -->
+        <div ng-if="isUserLoggedInSession==false" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 coSignInSignUpContainerDivClass">
+            <p class="coSignInSignUpInfoPClass">
+                <i class="fa fa-user accountSignUpSignInIconClass"></i> 
+                Please Log In / Sign Up to request for customize order
+            </p>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 coSignInSignUpBtnDivClass">
+                <button ng-click="goToSignUpSignInAccountSection('customizeorder')" class='btn coSignInSignUpBtnClass' id='coSignInSignUpBtnId'>
+                    Log In / Sign Up
+                </button>
+            </div>
+        </div>
 
+        <!-- create horizontally space div between -->
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 commonHorizontalSpaceDivClass"></div>
+
+    </div>
+
+    <!-- refresh web application data -->
+    <div id='refreshUserWebAppDataDivId' ng-controller="RefreshWebAppDataController" ng-init="refreshWebAppData()" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"></div>
+
+</div>
