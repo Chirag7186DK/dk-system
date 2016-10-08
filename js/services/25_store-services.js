42c7e34d6bdcb4c3f8a26094dcb873ca59f4639b
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-08 22:37:13 +0530

    code commited related to dk-system

--- js/services/25_store-services.js
+++ js/services/25_store-services.js
@@ -4,39 +4,31 @@
     angular.module('DKAPP').factory('StoreServices', StoreServices);
 
     // CJ defined this function 2016-06-22
-    function StoreServices($rootScope, LoaderServices){
+    function StoreServices($rootScope){
         try{
 
             var storeDetails = {};
 
             storeDetails.getStoreListDeliveryAreaBasedDessertsType = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Store/StoreListDeliveryAreaBasedDessertType", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             storeDetails.getDeliveryAreaBasedDessertsTypeList = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Store/DeliveryAreaBasedStoresConductDessertType", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             storeDetails.getStoreSummaryInfo = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Store/StoreSummaryInfo", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
 
             storeDetails.getStoreWorkingStyleDetails = function(apiParamJsonObj){
-                LoaderServices.showLoader();
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Store/StoreWorkingstyle", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    LoaderServices.hideLoader();
                 });
                 return promiseObject;
             };
@@ -51,11 +43,9 @@
                 // fetch param data from session
                 var paramDataObj = getParamObjStoreDeliveryFeeApplicableMsgOnDeliveryArea();
                 if(paramDataObj!==false && jQuery.isEmptyObject(paramDataObj)===false){
-                    LoaderServices.showLoader();
                     var apiParamJsonObj = {};
                     apiParamJsonObj['dkParamDataArr'] = paramDataObj;
                     communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Store/StoreDeliveryFeeApplicableDeliveryArea", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                        LoaderServices.hideLoader();
                         $rootScope.$apply(function(){
                             var storeDeliveryFeeApplicableMsg = '';
                             if(retResponseJson!==false && retResponseJson!==undefined && retResponseJson!==''){
