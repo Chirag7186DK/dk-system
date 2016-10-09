2cf51c3279fc261f782117b8d813c4483da62193
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-09 08:31:30 +0530

    code commited related to dk-system

--- js/services/26_ratingreview-services.js
+++ js/services/26_ratingreview-services.js
@@ -6,17 +6,6 @@
     // CJ defined this function 2016-04-22
     function RatingReviewServices(){
         try{
-
-            var ratingReviewDetails = {};
-
-            ratingReviewDetails.getStoreAllUserRatingReviewed = function(apiParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/StoreAllUserRating", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            return ratingReviewDetails;
-
         }catch(ex){
             console.log("problem in Rating/Review services ex=>"+ex);
             return false;
