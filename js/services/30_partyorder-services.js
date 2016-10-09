1dc141cbcd0946669c0b84ff327e6a305d37fb98
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-09 09:11:07 +0530

    code commited related to dk-system

--- js/services/30_partyorder-services.js
+++ js/services/30_partyorder-services.js
@@ -6,24 +6,6 @@
     // CJ defined this function 2016-07-20
     function PartyOrdersServices(){
         try{
-            var partyOrdersDetails = {
-                
-                addPartyOrderRequest:function(apiParamJsonObj){
-                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'POST', '', apiParamJsonObj).done(function(retResponseJson){
-                    });
-                    return promiseObject;
-                },
-                
-                getPartyOrdersList:function(apiParamJsonObj){
-                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/PartyOrders/ManagePartyOrders", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    });
-                    return promiseObject;
-                }
-                
-            };
-            
-            return partyOrdersDetails;
-            
         }catch(ex){
             console.log("Problem in PartyOrdersServices=>"+ex);
             return false;
