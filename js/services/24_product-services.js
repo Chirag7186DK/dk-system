219755ecc20bc155f96efd18b2e01617a73b7429
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-09 09:12:33 +0530

    code commited related to dk-system

--- js/services/24_product-services.js
+++ js/services/24_product-services.js
@@ -7,40 +7,6 @@
     function ProductServices(){
         try{
 
-            var productDetails = {};
-
-            productDetails.getProductTypeAllProductCategoryList = function(apiParamJsonObj){
-                var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeAllProductCategoryDetails", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            productDetails.getProductTypeProductCategoryFilterTypeList = function(apiParamJsonObj){
-                var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryFilterTypeList", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            productDetails.getProductTypeProductCategoryAllProductList = function(apiParamJsonObj){
-                var promiseObject = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryAllProductDetails", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            productDetails.getProductTypeProductCategoryProductDetails = function(apiParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductTypeProductCategoryProductDetails", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            productDetails.getProductDescriptionDetails = function(apiParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/Product/ProductDescriptionDetails", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            return productDetails;
-
         }catch(ex){
             console.log("problem in Product services ex=>"+ex);
             return false;
