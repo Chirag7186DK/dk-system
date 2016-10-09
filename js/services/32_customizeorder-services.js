417dea543d125c55c031932773f1ff6dcd907d28
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-09 09:14:12 +0530

    code commited related to dk-system

--- js/services/32_customizeorder-services.js
+++ js/services/32_customizeorder-services.js
@@ -6,24 +6,6 @@
     // CJ defined this function 2016-07-24
     function CustomizeOrdersServices(){
         try{
-
-            var customizeOrdersDetails = {
-
-                addCustomizeOrderRequest:function(apiParamJsonObj){
-                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'POST', '', apiParamJsonObj).done(function(retResponseJson){
-                    });
-                    return promiseObject;
-                },
-
-                getCustomizeOrdersList:function(apiParamJsonObj){
-                    var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/CustomizeOrders/ManageCustomizeOrders", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                    });
-                    return promiseObject;
-                }
-            };
-
-            return customizeOrdersDetails;
-
         }catch(ex){
             return false;
         }
