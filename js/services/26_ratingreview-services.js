8986a1b2dc7ce943f885bd2193f1c0bb33c4fba5
Chirag Jain <chirag.jain@digitaledu.net>
2016-10-09 08:31:20 +0530

    code commited related to dk-system

--- js/services/26_ratingreview-services.js
+++ js/services/26_ratingreview-services.js
@@ -9,38 +9,8 @@
 
             var ratingReviewDetails = {};
 
-            ratingReviewDetails.addUserRatingReviewProduct = function(apiParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/UserRatingReviewProduct", 'apiFile', 'POST', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            ratingReviewDetails.getStoreRatingReviewQuestions = function(apiParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/StoreRatingReviewQuestions", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
             ratingReviewDetails.getStoreAllUserRatingReviewed = function(apiParamJsonObj){
                 var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/StoreAllUserRating", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            ratingReviewDetails.getAllUserRatingReviewProduct = function(apiParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/AllUserRatingReviewProduct", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            ratingReviewDetails.getMaxRatingReviewProduct = function(apiParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/MaxRatingReviewProduct", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
-                });
-                return promiseObject;
-            };
-
-            ratingReviewDetails.getAverageRatingReviewProduct = function(apiParamJsonObj){
-                var promiseObject  = communicationWithAjax("dessertskhazana-services/dessertskhazanainnerservices/?r=api/v1/RatingReview/AverageRatingReviewProduct", 'apiFile', 'GET', '', apiParamJsonObj).done(function(retResponseJson){
                 });
                 return promiseObject;
             };
