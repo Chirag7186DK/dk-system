5a69da353a5c4b1e7fd29de73a2fd565ab9122a6
chirag <chirag@chirag-Vostro-1015>
2016-10-22 12:06:20 +0530

    code commited related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/UsersServicesV1.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/modules/api/modules/v1/services/implementations/UsersServicesV1.php
@@ -103,7 +103,6 @@ class UsersServicesV1 implements IUsersServicesV1{
                         $rspDetails['userDetails']['isOtpCodeSent'] = 'Y';
                         $rspDetails['userDetails']['isOtpCodeValidated'] = 'N';
                         $rspDetails['userDetails']['msgStr'] = $rtDataArr2['userDetails']['msgStr'];
-                        $rspDetails['userDetails']['smsData'] = $rtDataArr2['userDetails']['smsData'];
                     }else{
                         $rspDetails['userDetails']['isOtpCodeSent'] = 'N';
                         $rspDetails['userDetails']['isOtpCodeValidated'] = 'N';
