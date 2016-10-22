8e2fdc9c89b9b5d9f648e0f494f6312d13967d1a
chirag <chirag@chirag-Vostro-1015>
2016-10-22 13:13:29 +0530

    code commited related to dk-system

--- dessertskhazana-services/dessertskhazanainnerservices/protected/commonfunction/commonfunction.php
+++ dessertskhazana-services/dessertskhazanainnerservices/protected/commonfunction/commonfunction.php
@@ -413,7 +413,7 @@ class commonfunction{
         $rspDetails['userDetails']['isOtpCodeSent'] = 'N';
         if(count($paramDataArr)>0 && $paramDataArr!=false){
             // sending otp code and storing purpose
-            $otpCode = trim(getRandomOtpcode());
+            $otpCode = trim(utils :: getRandomOtpcode());
             $mobileStr = "XXXXXX".substr($paramDataArr['mobile'], -4);
             $paramDataArr['otpcode'] = $otpCode;
             $paramDataArr['sent_onmedium'] = 'mobile';
